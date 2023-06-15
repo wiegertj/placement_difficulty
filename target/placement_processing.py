084d3fcb82f5f822cde12186ca7f201df22c66d4
juliusw <uimmv@student.kit.edu>
2023-06-15 18:47:06 +0200

    Changed entropy normalization to by number of placements

--- target/placement_processing.py
+++ target/placement_processing.py
@@ -2,15 +2,16 @@ import json
 import os
 import pandas as pd
 from scipy.stats import entropy
+import math
+from Bio import Phylo
 
-def min_max_normalize(column):
-    min_val = column.min()
-    max_val = column.max()
-    normalized_column = (column - min_val) / (max_val - min_val)
-    return normalized_column
 
-def extract_entropy(jplace_file) -> pd.DataFrame:
-    print(jplace_file)
+def extract_entropy(jplace_file, tree_file) -> pd.DataFrame:
+
+    # Get branch count for normalization
+    tree = Phylo.read(os.path.join(os.pardir, "data/raw/reference_tree", tree_file), "newick")
+    num_branches = tree.count_terminals() - 1
+
     entropies = []
     with open(jplace_file, 'r') as f:
         jplace_data = json.load(f)
@@ -18,25 +19,17 @@ def extract_entropy(jplace_file) -> pd.DataFrame:
             sample_name = placement['n'][0]
             probabilities = placement['p']
             like_weight_ratios = [tup[2] for tup in probabilities]
-
-            entropy_val = entropy(like_weight_ratios)
+            entropy_val = entropy(like_weight_ratios) / math.log(num_branches)
             entropies.append((sample_name, entropy_val))
 
     df = pd.DataFrame(entropies, columns=['sampleId', 'Entropy'])
-    df['Entropy'] = min_max_normalize(df['Entropy'])
     return df
+
 
 if __name__ == '__main__':
 
-    for file in ["neotrop_10k_epa_result.jplace", "bv_epa_result.jplace", "tara_epa_result.jplace"]:
+    for file, tree_file in [("neotrop_10k_epa_result.jplace", "neotrop.newick"), ("bv_epa_result.jplace", "bv.newick"),
+                            ("tara_epa_result.jplace", "tara.newick")]:
         jplace_file_path = os.path.join(os.pardir, "data/raw/placements", file)
-        df = extract_entropy(jplace_file_path)
+        df = extract_entropy(jplace_file_path, tree_file)
         df.to_csv(os.path.join(os.pardir, "data/processed/target", file.replace(".jplace", "") + "_entropy.csv"))
-
-
-
-
-
-
-
-
