8eeb82f6bea1551ac86128028c94f68b89e2fb88
juliusw <uimmv@student.kit.edu>
2023-06-15 18:45:04 +0200

    Tara as testset

--- prediction/predictor.py
+++ prediction/predictor.py
@@ -5,26 +5,27 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
-import plotly.express as px
 
-# Read CSV file into a pandas DataFrame
 df = pd.read_csv(os.path.join(os.pardir, "data/processed/final", "final_dataset.csv"))
 df.drop(axis=1, columns=['dataset', 'sampleId'], inplace=True)
 
 # Split the dataset into features (X) and target (y)
-X = df.drop(axis=1, columns=["Entropy", "std_ham_dist", "min_ham_dist", "max_ham_dist", "avg_ham_dist"]) # Select all columns except the last column (features)
-y = df["Entropy"]   # Select the last column (target)
+#X = df[["gap_fraction"]]
 
+# Tara as test
+df_test = df[df["tree_depth"] == 3.8795620000000017]
+df_train = df[df["tree_depth"] != 3.8795620000000017]
 
-print(df[df["Entropy"] == 0].shape)
-print(df.shape)
+X_train = df_train.drop(axis=1, columns=["Entropy"])
+y_train = df_train["Entropy"]
 
+X_test = df_test.drop(axis=1, columns=["Entropy"])
+y_test = df_test["Entropy"]
 
-# Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+#X = df.drop(axis=1, columns=["Entropy"]) # Select all columns except the last column (features)
+#y = df["Entropy"]   # Select the last column (target)
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Define the RandomForestRegressor model
 model = RandomForestRegressor()
 #model = GradientBoostingRegressor()
 
@@ -34,48 +35,38 @@ param_grid_GBR = {
     'max_depth': [10]
 }
 
-
-# Define the hyperparameter grid for GridSearchCV
 param_grid = {
-    'n_estimators': [400],
-    'max_depth': [10],
-    'min_samples_split': [5]
+    'n_estimators': [300],
+    'max_depth': [20],
+    'max_features': [10],
+    'min_samples_split': [10],
+    'min_samples_leaf': [5]
 }
 
-# Perform GridSearchCV for hyperparameter tuning
-grid_search = GridSearchCV(model, param_grid, cv=5)
+# GridSearch
+grid_search = GridSearchCV(model, param_grid, cv=8)
 grid_search.fit(X_train, y_train)
-
-# Get the best model from GridSearchCV
 best_model = grid_search.best_estimator_
 
-# Evaluate the best model on the holdout test set
+best_params = grid_search.best_params_
+print("Best Parameters:")
+for param, value in best_params.items():
+    print(f"{param}: {value}")
+
+# MSE of entropy prediction on testset
 y_pred = best_model.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
 rmse = math.sqrt(mse)
-print(y_test)
-print(y_pred)
 
 print(f"Root Mean Squared Error on test set: {rmse}")
 feature_importances = best_model.feature_importances_
 
-
+# Scale and print feature importances
 scaler = MinMaxScaler()
 normalized_importances = scaler.fit_transform(feature_importances.reshape(-1, 1)).flatten()
-
-# Create a DataFrame with feature names and importances
-importance_df = pd.DataFrame({'Feature': X.columns, 'Importance': normalized_importances})
-
-# Sort the feature importances in descending order
+importance_df = pd.DataFrame({'Feature': X_train.columns, 'Importance': normalized_importances})
 importance_df = importance_df.sort_values(by='Importance', ascending=False)
 
-# Print the sorted and normalized feature importances
 print("Feature Importances (Normalized):")
 for index, row in importance_df.iterrows():
     print(f"{row['Feature']}: {row['Importance']:.4f}")
-
-best_params = grid_search.best_params_
-
-print("Best Parameters:")
-for param, value in best_params.items():
-    print(f"{param}: {value}")
