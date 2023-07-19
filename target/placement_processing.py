9d4ef36d7c7f935427edc37acce83a893d22a12d
juliusw <uimmv@student.kit.edu>
2023-07-19 17:01:46 +0200

    parallel placement processing

--- target/placement_processing.py
+++ target/placement_processing.py
@@ -1,4 +1,5 @@
 import json
+import multiprocessing
 import os
 import pandas as pd
 import math
@@ -6,12 +7,45 @@ import io
 from scipy.stats import entropy
 from Bio import Phylo
 
+def process_placements(*args):
+        placement, tree, max_distance, min_distance, num_branches = args[0]
+        sample_name = placement['n'][0]
+        probabilities = placement['p']
+        like_weight_ratios = [tup[2] for tup in probabilities]
+        entropy_val = entropy(like_weight_ratios, base=2) / math.log2(num_branches)
 
-def extract_entropy(jplace_file, tree_file) -> pd.DataFrame:
+        # calculate drop in lwr between best two branches
+        drop = 0
+        if len(like_weight_ratios) > 1:
+            sorted_lwr = sorted(like_weight_ratios, reverse=True)
+            largest_lwr1, largest_lwr2 = sorted_lwr[:2]
+            drop = abs(largest_lwr1 - largest_lwr2)
+        else:
+            drop = 1
+
+        # branch distance between two best
+        branch_distance = 0
+        if len(like_weight_ratios) > 1:
+            tree = Phylo.read(io.StringIO(tree), "newick")
+            best_edge = probabilities[0][0]
+            second_best_edge = probabilities[1][0]
+
+            for clade in tree.find_clades():
+                if clade.name == "{" + str(best_edge) + "}":
+                    best_edge = clade
+                elif clade.name == "{" + str(second_best_edge) + "}":
+                    second_best_edge = clade
+            clade_distance = tree.distance(best_edge, second_best_edge)
+            if clade_distance != 0:
+                branch_distance = (clade_distance - min_distance) / (max_distance - min_distance)
+
+        return sample_name, entropy_val, drop, branch_distance
+
+
+def extract_targets(jplace_file, tree_file) -> pd.DataFrame:
     # Get branch count for normalization
     tree = Phylo.read(os.path.join(os.pardir, "data/raw/reference_tree", tree_file), "newick")
     num_branches = tree.count_terminals() - 1
-    print(jplace_file)
 
     entropies = []
     with open(jplace_file, 'r') as f:
@@ -28,37 +62,26 @@ def extract_entropy(jplace_file, tree_file) -> pd.DataFrame:
                 if (distance < min_distance) and (clade1.name != clade2.name):
                     min_distance = distance
 
-        for placement in jplace_data['placements']:
-            sample_name = placement['n'][0]
-            probabilities = placement['p']
-            like_weight_ratios = [tup[2] for tup in probabilities]
-            entropy_val = entropy(like_weight_ratios, base=2) / math.log2(num_branches)
+        # Compute each placement target in parallel
+        if multiprocessing.current_process().name == 'MainProcess':
+            multiprocessing.freeze_support()
 
-            # calculate drop in lwr between best two branches
-            drop = 0
-            if len(like_weight_ratios) > 1:
-                sorted_lwr = sorted(like_weight_ratios, reverse=True)
-                largest_lwr1, largest_lwr2 = sorted_lwr[:2]
-                drop = abs(largest_lwr1 - largest_lwr2)
-            else:
-                drop = 1
+        pool = multiprocessing.Pool()
+        results = pool.imap_unordered(process_placements, [(placement, jplace_data["tree"], max_distance, min_distance, num_branches) for placement in jplace_data['placements']])
 
-            # branch distance between two best
-            branch_distance = 0
-            if len(like_weight_ratios) > 1:
-                tree = Phylo.read(io.StringIO(jplace_data["tree"]), "newick")
-                best_edge = probabilities[0][0]
-                second_best_edge = probabilities[1][0]
+        counter = 0
+        for result in results:
+            entropies.append(result)
+            counter +=1
+            if counter % 50 == 0:
+                print("Processed: " + str(counter) + " placements of " + jplace_file)
 
-                clade_distance = 0
-                clade_distance = tree.distance(best_edge, second_best_edge)
-                if clade_distance != 0:
-                    branch_distance = (clade_distance - min_distance) / (max_distance - min_distance)
-
-            entropies.append((sample_name, entropy_val, drop, branch_distance))
+        pool.close()
+        pool.join()
 
     df = pd.DataFrame(entropies, columns=['sampleId', 'entropy', "lwr_drop", "branch_dist_best_two_placements"])
     return df
+
 
 
 if __name__ == '__main__':
@@ -66,5 +89,5 @@ if __name__ == '__main__':
     for file, tree_file in [("neotrop_10k_epa_result.jplace", "neotrop.newick"), ("bv_epa_result.jplace", "bv.newick"),
                             ("tara_epa_result.jplace", "tara.newick")]:
         jplace_file_path = os.path.join(os.pardir, "data/raw/placements", file)
-        df = extract_entropy(jplace_file_path, tree_file)
+        df = extract_targets(jplace_file_path, tree_file)
         df.to_csv(os.path.join(os.pardir, "data/processed/target", file.replace(".jplace", "") + "_entropy.csv"))
--- target/placement_processing_loo.py
+++ target/placement_processing_loo.py
@@ -1,5 +1,6 @@
 import io
 import math
+import multiprocessing
 import os
 import json
 import pandas as pd
@@ -7,77 +8,93 @@ from Bio import Phylo
 from scipy.stats import entropy
 
 
+def extract_targets(*args):
+    root, file = args[0]
+    file_path = os.path.join(root, file)
+
+    with open(file_path, 'r') as f:
+        print(file_path)
+        data = json.load(f)
+
+        file_path = os.path.join(root, file)
+        folder_name = os.path.basename(os.path.dirname(file_path))
+        tree_name = '_'.join(folder_name.split('_')[:2])
+
+        # Get branch count for normalization
+        tree = Phylo.read(os.path.join(os.pardir, "data/raw/reference_tree", tree_name + ".newick"),
+                          "newick")
+        num_branches = tree.count_terminals() - 1
+
+        # Extract placement likelihood weight ratios
+        sample_name = ""
+        for placement in data['placements']:
+            sample_name = placement['n'][0]
+            probabilities = placement['p']
+            like_weight_ratios = [tup[2] for tup in probabilities]
+            entropy_val = entropy(like_weight_ratios, base=2) / math.log2(num_branches)
+
+            # calculate drop in lwr between best two branches
+            drop = 0
+            if len(like_weight_ratios) > 1:
+                sorted_lwr = sorted(like_weight_ratios, reverse=True)
+                largest_lwr1, largest_lwr2 = sorted_lwr[:2]
+                drop = abs(largest_lwr1 - largest_lwr2)
+            else:
+                drop = 1
+
+            # branch distance between two best
+            branch_distance = 0
+            if len(like_weight_ratios) > 1:
+                tree = Phylo.read(io.StringIO(data["tree"]), "newick")
+                best_edge = probabilities[0][0]
+                second_best_edge = probabilities[1][0]
+
+                for clade in tree.find_clades():
+                    if clade.name == "{" + str(best_edge) + "}":
+                        best_edge = clade
+                    elif clade.name == "{" + str(second_best_edge) + "}":
+                        second_best_edge = clade
+
+                max_distance = 0
+                min_distance = float("inf")
+                for clade1 in tree.find_clades():
+                    for clade2 in tree.find_clades():
+                        distance = tree.distance(clade1, clade2)
+                        if distance > max_distance:
+                            max_distance = distance
+                        if (distance < min_distance) and (clade1.name != clade2.name):
+                            min_distance = distance
+
+                clade_distance = 0
+                clade_distance = tree.distance(best_edge, second_best_edge)
+                if clade_distance != 0:
+                    branch_distance = (clade_distance - min_distance) / (max_distance - min_distance)
+
+            return tree_name, sample_name, entropy_val, drop, branch_distance
+
+
 def extract_jplace_info(directory):
-    results = []
+    targets = []
+    file_list = [(root, file) for root, dirs, files in os.walk(directory) for file in files if
+                 file.endswith('.jplace')]
 
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file.endswith('.jplace'):
-                file_path = os.path.join(root, file)
-                with open(file_path, 'r') as f:
-                    print(file_path)
-                    data = json.load(f)
+    pool = multiprocessing.Pool()
+    results = pool.imap_unordered(extract_targets, file_list)
 
-                    file_path = os.path.join(root, file)
-                    folder_name = os.path.basename(os.path.dirname(file_path))
-                    tree_name = '_'.join(folder_name.split('_')[:2])
+    for result in results:
+        targets.append(result)
 
-                    # Get branch count for normalization
-                    tree = Phylo.read(os.path.join(os.pardir, "data/raw/reference_tree", tree_name + ".newick"),
-                                      "newick")
-                    num_branches = tree.count_terminals() - 1
-
-                    # Extract placement likelihood weight ratios
-                    sample_name = ""
-                    for placement in data['placements']:
-                        sample_name = placement['n'][0]
-                        probabilities = placement['p']
-                        like_weight_ratios = [tup[2] for tup in probabilities]
-                        entropy_val = entropy(like_weight_ratios, base=2) / math.log2(num_branches)
-
-                        # calculate drop in lwr between best two branches
-                        drop = 0
-                        if len(like_weight_ratios) > 1:
-                            sorted_lwr = sorted(like_weight_ratios, reverse=True)
-                            largest_lwr1, largest_lwr2 = sorted_lwr[:2]
-                            drop = abs(largest_lwr1 - largest_lwr2)
-                        else:
-                            drop = 1
-
-                        # branch distance between two best
-                        branch_distance = 0
-                        if len(like_weight_ratios) > 1:
-                            tree = Phylo.read(io.StringIO(data["tree"]), "newick")
-                            best_edge = probabilities[0][0]
-                            second_best_edge = probabilities[1][0]
-
-                            for clade in tree.find_clades():
-                                if clade.name == "{" + str(best_edge) + "}":
-                                    best_edge = clade
-                                elif clade.name == "{" + str(second_best_edge) + "}":
-                                    second_best_edge = clade
-
-                            max_distance = 0
-                            min_distance = float("inf")
-                            for clade1 in tree.find_clades():
-                                for clade2 in tree.find_clades():
-                                    distance = tree.distance(clade1, clade2)
-                                    if distance > max_distance:
-                                        max_distance = distance
-                                    if (distance < min_distance) and (clade1.name != clade2.name):
-                                        min_distance = distance
-
-                            clade_distance = 0
-                            clade_distance = tree.distance(best_edge, second_best_edge)
-                            if clade_distance != 0:
-                                branch_distance = (clade_distance - min_distance) / (max_distance - min_distance)
-
-                        results.append((tree_name, sample_name, entropy_val, drop, branch_distance))
+    pool.close()
+    pool.join()
 
     df = pd.DataFrame(results,
                       columns=["dataset", "sampleId", "entropy", "lwr_drop", "branch_dist_best_two_placements"])
     df.to_csv(os.path.join(os.pardir, "data/processed/target", "loo_result_entropy.csv"), index=False)
 
 
-loo_results_path = os.path.join(os.pardir, "data/processed/loo_results")
-extract_jplace_info(loo_results_path)
+if __name__ == '__main__':
+    if multiprocessing.current_process().name == 'MainProcess':
+        multiprocessing.freeze_support()
+
+    loo_results_path = os.path.join(os.pardir, "data/processed/loo_results")
+    extract_jplace_info(loo_results_path)
