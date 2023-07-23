0dccf5f6bf9bf9fe415f2bf2d8e4abac91611375
juliusw <uimmv@student.kit.edu>
2023-07-23 20:29:40 +0200

    Added Exeception Handling

--- scripts/get_file_names.py
+++ scripts/get_file_names.py
@@ -13,7 +13,7 @@ difficulties_df = pd.read_csv(difficulties_path, index_col=False, usecols=lambda
 # Filter out already used LOO-files
 if os.path.exists(os.path.join(os.pardir, "data/loo_selection.csv")):
     df_used = pd.read_csv(os.path.join(os.pardir, "data/loo_selection.csv"))
-    names_used = df_used["verbose name"].unique()
+    names_used = df_used["verbose_name"].unique()
     difficulties_df = difficulties_df[~difficulties_df["verbose name"].isin(names_used)]
 
 difficulty_ranges = np.arange(0.1, 1.1, 0.1)
