632f83ad375a1aa0278bac1d988efd08fbcd3cd5
juliusw <uimmv@student.kit.edu>
2023-07-23 17:34:57 +0200

    Parallel min/max computation

--- target/placement_processing.py
+++ target/placement_processing.py
@@ -7,6 +7,8 @@ import io
 from scipy.stats import entropy
 from Bio import Phylo
 from joblib import Parallel, delayed
+from multiprocessing import Pool
+
 
 def calculate_distance(tree, clade):
     clades = tree.find_clades()
@@ -17,39 +19,43 @@ def calculate_distance(tree, clade):
             distances.append(distance)
     return distances
 
+def get_min_max(list):
+    return min(list), max(list)
+
+
 def process_placements(*args):
-        placement, tree, max_distance, min_distance, num_branches = args[0]
-        sample_name = placement['n'][0]
-        probabilities = placement['p']
-        like_weight_ratios = [tup[2] for tup in probabilities]
-        entropy_val = entropy(like_weight_ratios, base=2) / math.log2(num_branches)
+    placement, tree, max_distance, min_distance, num_branches = args[0]
+    sample_name = placement['n'][0]
+    probabilities = placement['p']
+    like_weight_ratios = [tup[2] for tup in probabilities]
+    entropy_val = entropy(like_weight_ratios, base=2) / math.log2(num_branches)
 
-        # calculate drop in lwr between best two branches
-        drop = 0
-        if len(like_weight_ratios) > 1:
-            sorted_lwr = sorted(like_weight_ratios, reverse=True)
-            largest_lwr1, largest_lwr2 = sorted_lwr[:2]
-            drop = abs(largest_lwr1 - largest_lwr2)
-        else:
-            drop = 1
+    # calculate drop in lwr between best two branches
+    drop = 0
+    if len(like_weight_ratios) > 1:
+        sorted_lwr = sorted(like_weight_ratios, reverse=True)
+        largest_lwr1, largest_lwr2 = sorted_lwr[:2]
+        drop = abs(largest_lwr1 - largest_lwr2)
+    else:
+        drop = 1
 
-        # branch distance between two best
-        branch_distance = 0
-        if len(like_weight_ratios) > 1:
-            tree = Phylo.read(io.StringIO(tree), "newick")
-            best_edge = probabilities[0][0]
-            second_best_edge = probabilities[1][0]
+    # branch distance between two best
+    branch_distance = 0
+    if len(like_weight_ratios) > 1:
+        tree = Phylo.read(io.StringIO(tree), "newick")
+        best_edge = probabilities[0][0]
+        second_best_edge = probabilities[1][0]
 
-            for clade in tree.find_clades():
-                if clade.name == "{" + str(best_edge) + "}":
-                    best_edge = clade
-                elif clade.name == "{" + str(second_best_edge) + "}":
-                    second_best_edge = clade
-            clade_distance = tree.distance(best_edge, second_best_edge)
-            if clade_distance != 0:
-                branch_distance = (clade_distance - min_distance) / (max_distance - min_distance)
+        for clade in tree.find_clades():
+            if clade.name == "{" + str(best_edge) + "}":
+                best_edge = clade
+            elif clade.name == "{" + str(second_best_edge) + "}":
+                second_best_edge = clade
+        clade_distance = tree.distance(best_edge, second_best_edge)
+        if clade_distance != 0:
+            branch_distance = (clade_distance - min_distance) / (max_distance - min_distance)
 
-        return sample_name, entropy_val, drop, branch_distance
+    return sample_name, entropy_val, drop, branch_distance
 
 
 def extract_targets(jplace_file, tree_file) -> pd.DataFrame:
@@ -67,25 +73,29 @@ def extract_targets(jplace_file, tree_file) -> pd.DataFrame:
 
         distances = Parallel(n_jobs=num_jobs)(delayed(calculate_distance)(*args) for args in all_clades)
 
-        distances = sum(distances, [])
+        print("Calculates list of distances ... start finding min/max")
 
+        with Pool(processes=num_jobs) as pool:
+            results = pool.map(get_min_max, distances)
 
-        max_distance = max(distances)
-        min_distance = min(distances)
+        # Extract the minimum and maximum values from the results
+        min_distance = min(result[0] for result in results)
+        max_distance = max(result[1] for result in results)
 
         print("Calculates max distance: " + str(max_distance))
         print("Calculates min distance: " + str(min_distance))
 
         # Compute each placement target in parallel
 
-
         pool = multiprocessing.Pool()
-        results = pool.imap_unordered(process_placements, [(placement, jplace_data["tree"], max_distance, min_distance, num_branches) for placement in jplace_data['placements']])
+        results = pool.imap_unordered(process_placements,
+                                      [(placement, jplace_data["tree"], max_distance, min_distance, num_branches) for
+                                       placement in jplace_data['placements']])
 
         counter = 0
         for result in results:
             entropies.append(result)
-            counter +=1
+            counter += 1
             if counter % 50 == 0:
                 print("Processed: " + str(counter) + " placements of " + jplace_file)
 
@@ -94,7 +104,6 @@ def extract_targets(jplace_file, tree_file) -> pd.DataFrame:
 
     df = pd.DataFrame(entropies, columns=['sampleId', 'entropy', "lwr_drop", "branch_dist_best_two_placements"])
     return df
-
 
 
 if __name__ == '__main__':
