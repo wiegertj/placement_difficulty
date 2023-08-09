446a437920ac16afdb0b9df39191219ab629df5e
juliusw <uimmv@student.kit.edu>
2023-08-09 17:39:37 +0200

    config

--- features/perc_hashing_msa_query.py
+++ features/perc_hashing_msa_query.py
@@ -58,6 +58,7 @@ def compute_perceptual_hash_distance(msa_file):
     # Skip already processed
     potential_path = os.path.join(os.pardir, "data/processed/features",
                                   msa_file.replace("_reference.fasta", "") + "16_msa_perc_hash_dist" + ".csv")
+    print(potential_path)
     if os.path.exists(potential_path):
         print("Skipped: " + msa_file + " already processed")
         return 0
