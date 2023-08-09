33cf9fecad63026cabcad9658eb2e0421e56480f
juliusw <uimmv@student.kit.edu>
2023-08-09 22:59:03 +0200

    config

--- features/perc_hashing_msa_query.py
+++ features/perc_hashing_msa_query.py
@@ -37,6 +37,7 @@ def compute_dct_sign_only_hash(sequence):
 
     dct_coeffs = dct(dct(image, axis=0), axis=1)
     sign_only_sequence = np.sign(dct_coeffs)
+    size_ = 16
     if sign_only_sequence.shape[0] >= 64 and sign_only_sequence.shape[1] >= 64:
         size_ = 64
     if sign_only_sequence.shape[0] >= 32 and sign_only_sequence.shape[1] >= 32:
