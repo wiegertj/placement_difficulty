012de77e5ac781bb6eb7fdc9dd0ecffc218e268f
juliusw <uimmv@student.kit.edu>
2023-08-29 16:32:30 +0200

    Mutation counting

--- features/perc_hashing_msa_query.py
+++ features/perc_hashing_msa_query.py
@@ -97,7 +97,6 @@ def compute_image_distances(msa_file):
 
         numeric_query = dna_to_numeric(record_query.seq)
         image_query = encode_dna_as_image(numeric_query)
-        print(image_query)
         image_query = image_query.astype(np.uint8)
 
         #contours, _ = cv2.findContours(image_query, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
