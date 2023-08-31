1cef1059503cd60a35610baa7f7efec3dcc3ecee
juliusw <uimmv@student.kit.edu>
2023-08-31 20:42:33 +0200

    Mutation counting and image comparison added

--- features/tree_uncertainty.py
+++ features/tree_uncertainty.py
@@ -5,25 +5,22 @@ import os
 loo_selection = pd.read_csv(os.path.join(os.pardir, "data/loo_selection.csv"))
 filenames = loo_selection['verbose_name'].str.replace(".phy", ".newick").tolist()
 
-
 for file in filenames:
     if not os.path.exists(os.path.join(os.pardir, "data/raw/reference_tree", file)):
         print("Not found " + file)
         filenames.remove(file)
 
-
-
-
-# Loop through each tree file
+counter = 0
 for tree_filename in filenames:
+    counter += 1
+    print(counter)
     tree_path = os.path.join(os.pardir, "data/raw/reference_tree", tree_filename)
     msa_filepath = os.path.join(os.pardir, "data/raw/msa", tree_filename.replace(".newick", "_reference.fasta"))
     model_path = os.path.join(os.pardir, "data/processed/loo", tree_filename.replace(".newick", "") + "_msa_model.txt")
-    bootstrap_filepath =  os.path.join(os.pardir, "data/raw/msa", tree_filename.replace(".newick", "_reference.fasta") + ".raxml.bootstraps")
-    # Output prefix for each tree
+    bootstrap_filepath = os.path.join(os.pardir, "data/raw/msa",
+                                      tree_filename.replace(".newick", "_reference.fasta") + ".raxml.bootstraps")
     output_prefix = tree_filename.split(".")[0]  # Using the filename as the prefix
 
-    # Command to run RAxML for bootstrap analysis
     raxml_command = [
         "raxml-ng",
         "--bootstrap",
@@ -33,27 +30,14 @@ for tree_filename in filenames:
         "--redo"
     ]
 
-    # Run RAxML using subprocess
     subprocess.run(" ".join(raxml_command), shell=True)
 
     print(f"Bootstrap analysis for {tree_filename} completed.")
 
-
     raxml_command = ["raxml-ng",
-        "--support",
-        f"--tree {tree_path}",
-        f"--bs-trees {bootstrap_filepath}",
-        "--redo"   ]
-
+                     "--support",
+                     f"--tree {tree_path}",
+                     f"--bs-trees {bootstrap_filepath}",
+                     "--redo"]
 
     subprocess.run(" ".join(raxml_command), shell=True)
-
-# Read the bootstrap support values from the output file
-    with open(f"{output_prefix}.raxml.support", "r") as support_file:
-        for line in support_file:
-            bootstrap_supports.append(float(line.strip()))
-
-# Calculate the average bootstrap support for the original tree
-average_support = sum(bootstrap_supports) / len(bootstrap_supports)
-
-print(f"Average support of branches in the original tree: {average_support:.2f}")
