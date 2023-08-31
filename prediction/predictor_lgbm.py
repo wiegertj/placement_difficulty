17416a3716dd1bb18a5dea66e865f388381bf879
juliusw <uimmv@student.kit.edu>
2023-08-31 17:20:22 +0200

    Mutation counting and image comparison added

--- prediction/predictor_lgbm.py
+++ prediction/predictor_lgbm.py
@@ -66,7 +66,7 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=10, shapley_calc=True, targets=
        # 'min_child_samples': [20, 50]
     #}
 
-    tuner = LGBMTuner(metric='mse', trials=200, visualization=True, verbostity=2)  # <- the only required argument
+    tuner = LGBMTuner(metric='mse', trials=100, visualization=True, verbostity=2)  # <- the only required argument
     tuner.fit(X_train, y_train)
     # check the optimization log in the console.
     #pred = tuner.predict(test)
