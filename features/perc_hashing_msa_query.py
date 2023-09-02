3c472f1ad019566c72996fcc7a310ef818e3b631
juliusw <uimmv@student.kit.edu>
2023-09-02 21:56:01 +0200

    Normalization of features

--- features/perc_hashing_msa_query.py
+++ features/perc_hashing_msa_query.py
@@ -295,8 +295,8 @@ def compute_perceptual_hash_distance(msa_file):
                     kmer_sims10.append(kmer_sim10)
                     kmer_sims25.append(kmer_sim25)
                     lcs_values.append(lcs)
-                    print(normalized_msa_dct_coeff.flatten())
-                    coeff_dist = np.linalg.norm(normalized_msa_dct_coeff.flatten(), normalized_query_dct_coeff.flatten())
+                    difference = normalized_msa_dct_coeff.flatten() - normalized_query_dct_coeff.flatten()
+                    coeff_dist = np.linalg.norm(difference)
                     coeff_dists.append(coeff_dist)
                 else:
                     return 0
