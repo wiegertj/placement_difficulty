26f25926332ed10040293bea1f6984d37a9cbdff
juliusw <uimmv@student.kit.edu>
2023-09-03 09:36:46 +0200

    Normalization of features

--- features/query_processing.py
+++ features/query_processing.py
@@ -180,7 +180,6 @@ if __name__ == '__main__':
 
     loo_selection = pd.read_csv(os.path.join(os.pardir, "data/loo_selection.csv"))
     filenames = loo_selection['verbose_name'].str.replace(".phy", "_query.fasta").tolist()
-    filenames = filenames[:2]
 
     if feature_config.INCUDE_TARA_BV_NEO:
         filenames = filenames + ["bv_query.fasta", "neotrop_query_10k.fasta", "tara_query.fasta"]
