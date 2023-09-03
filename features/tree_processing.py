f5ffa223132f885593770ef8950da5d8f8151dce
juliusw <uimmv@student.kit.edu>
2023-09-03 11:27:34 +0200

    Normalization of features

--- features/tree_processing.py
+++ features/tree_processing.py
@@ -49,7 +49,7 @@ def analyze_newick_tree(newick_tree, tree_file) -> tuple:
 
     all_nodes = tree.traverse()
     inner_nodes = [node for node in all_nodes if not node.is_leaf()]
-    inner_nodes = [b / tree_len_1 for b in inner_nodes]
+    inner_nodes = [b.dist / tree_len_1 for b in inner_nodes]
     print(len(inner_nodes))
     inner_branch_lengths = [node.dist for node in inner_nodes]
     average_branch_length_inner = sum(inner_branch_lengths) / len(inner_nodes)
