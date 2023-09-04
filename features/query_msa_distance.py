4675c778c44f90b444d412b2242dbf49720684ff
juliusw <uimmv@student.kit.edu>
2023-09-04 22:25:10 +0200

    Normalization of features

--- features/query_msa_distance.py
+++ features/query_msa_distance.py
@@ -4,6 +4,7 @@ import os
 import statistics
 from Bio import SeqIO
 from scipy.stats import kurtosis, skew
+import pylcs
 
 
 def compute_hamming_distance(msa_file, query_file) -> list:
@@ -25,9 +26,12 @@ def compute_hamming_distance(msa_file, query_file) -> list:
         if counter % 50 == 0:
             print(counter)
         distances = []
+        lcss = []
         for record_msa in SeqIO.parse(os.path.join(os.pardir, "data/raw/msa", msa_file), 'fasta'):
             if record_msa.id != record_query.id:
                 distance = sum(ch1 != ch2 for ch1, ch2 in zip(record_query.seq, record_msa.seq))
+                lcs = pylcs.lcs_sequence_length(record_query.seq, record_msa.seq)
+                lcss.append(lcs)
                 distances.append(distance)
 
         max_ham = max(distances)
@@ -50,6 +54,26 @@ def compute_hamming_distance(msa_file, query_file) -> list:
         rel_avg_ham = avg_ham / len(record_query.seq)
         rel_std_ham = std_ham / len(record_query.seq)
 
+        max_lcss = max(lcss)
+        min_lcss = min(lcss)
+        avg_lcss = sum(lcss) / len(lcss)
+        std_lcss = statistics.stdev(lcss)
+        if avg_lcss != 0:
+            cv_lcss = std_lcss / avg_lcss
+        else:
+            cv_lcss = 0
+
+        if min(lcss) == max(lcss):
+            sk_lcss = 0
+        else:
+            sk_lcss = skew([(x - min(lcss)) / (max(lcss) - min(lcss)) for x in
+                           lcss])
+        kur_lcss = kurtosis(lcss, fisher=True)
+        rel_max_lcss = max_lcss / len(record_query.seq)
+        rel_min_lcss = min_lcss / len(record_query.seq)
+        rel_avg_lcss = avg_lcss / len(record_query.seq)
+        rel_std_lcss = std_lcss / len(record_query.seq)
+
         name = ""
 
         if msa_file == "neotrop_reference.fasta":
@@ -61,7 +85,8 @@ def compute_hamming_distance(msa_file, query_file) -> list:
         else:
             name = msa_file.replace("_msa.fasta", "")
 
-        results.append((name, record_query.id, rel_min_ham, rel_max_ham, rel_avg_ham, rel_std_ham, cv_ham, sk_ham, kur_ham))
+        results.append((name, record_query.id, rel_min_ham, rel_max_ham, rel_avg_ham, rel_std_ham, cv_ham, sk_ham, kur_ham,
+                        rel_min_lcss, rel_max_lcss, rel_avg_lcss, rel_std_lcss, cv_lcss, sk_lcss, kur_lcss))
 
     return results
 
@@ -91,10 +116,10 @@ if __name__ == '__main__':
     counter_msa = 0
     for msa_file in filenames:
         print(msa_file)
-        if os.path.isfile(os.path.join(os.pardir, "data/processed/features",
-                                       msa_file.replace("reference.fasta", "msa_dist.csv"))):
-            print("Found " + msa_file + " skipped")
-            continue
+        #if os.path.isfile(os.path.join(os.pardir, "data/processed/features",
+         #                              msa_file.replace("reference.fasta", "msa_dist.csv"))):
+          #  print("Found " + msa_file + " skipped")
+           # continue
         #if len(next(SeqIO.parse(os.path.join(os.pardir, "data/raw/msa", msa_file), 'fasta').records).seq) > 15000:
          #   print("Skipped " + msa_file + " too large")
           #  continue
@@ -104,6 +129,9 @@ if __name__ == '__main__':
         result_tmp = compute_hamming_distance(msa_file, msa_file.replace("reference.fasta", "query.fasta"))
 
         df = pd.DataFrame(result_tmp, columns=['dataset', 'sampleId', 'min_ham_dist', 'max_ham_dist', 'avg_ham_dist',
-                                               'std_ham_dist', "cv_ham_dist", "sk_ham_dist", "kur_ham_dist"])
+                                               'std_ham_dist', "cv_ham_dist", "sk_ham_dist", "kur_ham_dist",
+                                               'min_lcss_dist', 'max_lcss_dist', 'avg_lcss_dist',
+                                               'std_lcss_dist', "cv_lcss_dist", "sk_lcss_dist", "kur_lcss_dist"
+                                               ])
         df.to_csv(os.path.join(os.pardir, "data/processed/features",
                                msa_file.replace("_reference.fasta", "") + "_msa_dist.csv"))
