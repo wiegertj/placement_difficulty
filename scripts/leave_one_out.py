217a24870f666037496f94af33bd6eb750ccc4a4
juliusw <uimmv@student.kit.edu>
2023-09-05 11:59:15 +0200

    Changed condif

--- scripts/leave_one_out.py
+++ scripts/leave_one_out.py
@@ -240,10 +240,33 @@ for msa_name in filtered_filenames:
                         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
                         stdout, stderr = process.communicate()
 
+                        while True:
+                            # Read a line from stdout
+                            stdout_line = process.stdout.readline()
+
+                            # Read a line from stderr
+                            stderr_line = process.stderr.readline()
+
+                            # Check if both streams are empty (process has finished)
+                            if not stdout_line and not stderr_line:
+                                break
+
+                            # Print stdout if available
+                            if stdout_line:
+                                print("stdout:", stdout_line.strip())
+
+                            # Print stderr if available
+                            if stderr_line:
+                                print("stderr:", stderr_line.strip())
+
+                        # Wait for the process to complete
+                        process.wait()
+
                         if process.returncode == 0:
                             print("Quartet process completed successfully.")
                             print("Output:")
                             print(stdout)
+
 
                             model_path_epa = os.path.join(os.pardir, "data/processed/loo",
                                                           msa_name + "_msa_" + str(
@@ -255,9 +278,7 @@ for msa_name in filtered_filenames:
                             print(stderr)
                             continue
 
-                    except FileNotFoundError:
-                        print("RAxML-ng executable not found. Please make sure RAxML-ng is installed and in the system")
-                    #quartet_distance = tqdist.quartet_distance(original_tree_path, tree_path)
+
 
 
                     # BSD distance
