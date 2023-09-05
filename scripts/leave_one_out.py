58f81be6eef941d08e3c28bf9414630835c1732a
juliusw <uimmv@student.kit.edu>
2023-09-05 12:17:26 +0200

    Changed condif

--- scripts/leave_one_out.py
+++ scripts/leave_one_out.py
@@ -235,7 +235,7 @@ for msa_name in filtered_filenames:
                     print("Started quartet computation")
                     print(os.path.abspath(original_tree_path).replace(".newick", to_query + ".newick"))
                     print(os.path.abspath(tree_path))
-                    command = ["/home/wiegerjs/tqDist-1.0.2/bin/quartet_dist", "-v",tree_path, os.path.abspath(original_tree_path).replace(".newick", to_query + ".newick")]
+                    command = ["/home/wiegerjs/tqDist-1.0.2/bin/quartet_dist", "-v", "", tree_path, "", os.path.abspath(original_tree_path).replace(".newick", to_query + ".newick")]
                     try:
                         print(command)
                         process = subprocess.check_output(command,stderr=subprocess.STDOUT,
