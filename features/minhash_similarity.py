0d22ce1beeabb3378e1ebadae17739c7f952ff9b
juliusw <uimmv@student.kit.edu>
2023-09-06 22:34:53 +0200

    Minhash features

--- features/minhash_similarity.py
+++ features/minhash_similarity.py
@@ -46,32 +46,36 @@ def multiprocess_minhash_similarity(msa_filename):
     results = []
 
     for query_record in SeqIO.parse(query_filename, 'fasta'):
-        query_jacc_sims25 = []
-        query_jacc_sims50 = []
-        for seq_record in msa:
-            seq_rec = ''.join(map(replace_ambiguity_chars, seq_record.seq)).replace("-", "")
-            query_seq = ''.join(map(replace_ambiguity_chars, query_record.seq)).replace("-", "")
-            mh1 = sourmash.MinHash(n=0, ksize=25, scaled=1)
-            mh1.add_sequence(seq_rec.replace("-", ""))
-            mh2 = sourmash.MinHash(n=0, ksize=25, scaled=1)
-            mh2.add_sequence(query_seq.replace("-", ""))
-            query_jacc_sim25 = round(mh1.jaccard(mh2), 2)
-            mh1 = sourmash.MinHash(n=0, ksize=50, scaled=1)
-            mh1.add_sequence(seq_rec.replace("-", ""))
-            mh2 = sourmash.MinHash(n=0, ksize=50, scaled=1)
-            mh2.add_sequence(query_seq.replace("-", ""))
-            query_jacc_sim50 = round(mh1.jaccard(mh2), 2)
-            query_jacc_sims25.append(query_jacc_sim25)
-            query_jacc_sims50.append(query_jacc_sim50)
-        mean25 = statistics.mean(query_jacc_sims25)
-        mean50 = statistics.mean(query_jacc_sims50)
-        std25 = statistics.stdev(query_jacc_sims25)
-        std50 = statistics.stdev(query_jacc_sims50)
-        min25 = min(query_jacc_sims25)
-        min50 = min(query_jacc_sims50)
-        max50 = max(query_jacc_sims50)
-        max25 = max(query_jacc_sims25)
-        results.append((msa_filename, query_record.id, mean25, mean50, std25, std50, min25, min50, max25, max50))
+        try:
+            query_jacc_sims25 = []
+            query_jacc_sims50 = []
+            for seq_record in msa:
+                    seq_rec = ''.join(map(replace_ambiguity_chars, seq_record.seq)).replace("-", "")
+                    query_seq = ''.join(map(replace_ambiguity_chars, query_record.seq)).replace("-", "")
+                    mh1 = sourmash.MinHash(n=0, ksize=25, scaled=1)
+                    mh1.add_sequence(seq_rec.replace("-", ""))
+                    mh2 = sourmash.MinHash(n=0, ksize=25, scaled=1)
+                    mh2.add_sequence(query_seq.replace("-", ""))
+                    query_jacc_sim25 = round(mh1.jaccard(mh2), 2)
+                    mh1 = sourmash.MinHash(n=0, ksize=50, scaled=1)
+                    mh1.add_sequence(seq_rec.replace("-", ""))
+                    mh2 = sourmash.MinHash(n=0, ksize=50, scaled=1)
+                    mh2.add_sequence(query_seq.replace("-", ""))
+                    query_jacc_sim50 = round(mh1.jaccard(mh2), 2)
+                    query_jacc_sims25.append(query_jacc_sim25)
+                    query_jacc_sims50.append(query_jacc_sim50)
+            mean25 = statistics.mean(query_jacc_sims25)
+            mean50 = statistics.mean(query_jacc_sims50)
+            std25 = statistics.stdev(query_jacc_sims25)
+            std50 = statistics.stdev(query_jacc_sims50)
+            min25 = min(query_jacc_sims25)
+            min50 = min(query_jacc_sims50)
+            max50 = max(query_jacc_sims50)
+            max25 = max(query_jacc_sims25)
+            results.append((msa_filename, query_record.id, mean25, mean50, std25, std50, min25, min50, max25, max50))
+        except ValueError:
+            print("Value Error occured, skipped")
+            continue
     return results
 
 
@@ -99,15 +103,10 @@ if __name__ == '__main__':
                                       file.replace("_reference.fasta", "") + "_minhash_0".replace("0.",
                                                                                                   "") + "_1000" + ".csv")
         if os.path.exists(potential_path):
-            filenames.remove(file)
-
-    pool = multiprocessing.Pool()
-    results = pool.imap(multiprocess_minhash_similarity, filenames)
-    counter = 0
-    for result in results:
-        counter += 1
-        print(counter)
-        df = pd.DataFrame(results,
+            print("Found, skipped")
+            continue
+        result = multiprocess_minhash_similarity(file)
+        df = pd.DataFrame(result,
                           columns=['dataset', 'sampleId', 'minhash_mean_25', 'minhash_mean_50', 'minhash_std_25',
                                    'minhash_std_50',
                                    'minhash_min_25', 'minhash_min_50', 'minhash_max_25', "minhash_max_50"])
@@ -116,6 +115,22 @@ if __name__ == '__main__':
                                                                                                 "") + "_1000.csv"),
                   index=False)
 
-    pool.close()
-    pool.join()
+
+   # pool = multiprocessing.Pool()
+    #results = pool.imap(multiprocess_minhash_similarity, filenames)
+    #counter = 0
+    #for result in results:
+     #   counter += 1
+      #  print(counter)
+       # df = pd.DataFrame(results,
+        #                  columns=['dataset', 'sampleId', 'minhash_mean_25', 'minhash_mean_50', 'minhash_std_25',
+         #                          'minhash_std_50',
+          #                         'minhash_min_25', 'minhash_min_50', 'minhash_max_25', "minhash_max_50"])
+        #df.to_csv(os.path.join(os.pardir, "data/processed/features",
+         #                      filenames.replace("_reference.fasta", "") + "_minhash_0".replace("0.",
+          #                                                                                      "") + "_1000.csv"),
+           #       index=False)
+
+    #pool.close()
+    #pool.join()
 
