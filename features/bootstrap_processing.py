227f421d33afd48839a014a06652fc20a58b0bd4
juliusw <uimmv@student.kit.edu>
2023-09-14 13:46:17 +0200

    Changed

--- features/bootstrap_processing.py
+++ features/bootstrap_processing.py
@@ -183,6 +183,8 @@ filenames = loo_selection['verbose_name'].str.replace(".phy", ".newick").tolist(
 results = []
 counter = 0
 
+filenames = filenames[:10]
+
 for file in filenames:
     counter += 1
     print(counter)
