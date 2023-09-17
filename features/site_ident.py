b4179ebeac5c4774f1d7b0eb881dde465b223f87
juliusw <uimmv@student.kit.edu>
2023-09-17 18:03:59 +0200

    Changed

--- features/site_ident.py
+++ features/site_ident.py
@@ -84,7 +84,7 @@ def calculate_imp_site(support_file_path, msa_filepath, name):
         # Iterate through all branches in the tree
         for node in phylo_tree.traverse("postorder"):
             if node.support is not None and not node.is_root() and not node.is_leaf():
-                print(len(node.children))
+                print(len(node.get_descendants()))
                 if node.support < min_support and len(node.get_descendants()) > (0.45 * phylo_tree.__len__()):
                     print("matched 0.45")
                     min_support = node.support
@@ -178,7 +178,7 @@ def calculate_imp_site(support_file_path, msa_filepath, name):
             site_freq_b_array = np.array(list(normalized_freq_b.values()))
 
             kl_divergence_value = abs(site_freq_a_array - site_freq_b_array)
-            kl_divergence_value = np.mean(kl_divergence_value)
+            kl_divergence_value = np.max(kl_divergence_value)
 
             #kl_divergence_value = entropy(site_freq_a_array, site_freq_b_array)
 
