1b3ee38e41949b54790738cef0cefe6b0e67e7cd
juliusw <uimmv@student.kit.edu>
2023-09-19 14:14:17 +0200

    Changed

--- features/site_ident.py
+++ features/site_ident.py
@@ -213,7 +213,7 @@ def calculate_imp_site(support_file_path, msa_filepath, name):
                 new_alignment_dup.append(record)
         SeqIO.write(new_alignment_dup, os.path.abspath(msa_filepath.replace("_reference", "_dedup_reference")), "fasta")
         msa_filepath = msa_filepath.replace("_reference", "_dedup_reference")
-        alignment = SeqIO.read(msa_filepath, format="fasta")
+        alignment = AlignIO.MultipleSeqAlignment(new_alignment_dup)
         modified_sequences = []
 
         print("Binary sum \n")
