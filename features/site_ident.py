e79101397fffc67e56b82ff5d0cea1fc346b81af
juliusw <uimmv@student.kit.edu>
2023-09-19 17:09:11 +0200

    Changed

--- features/site_ident.py
+++ features/site_ident.py
@@ -298,12 +298,12 @@ def calculate_imp_site(support_file_path, msa_filepath, name):
             df_py = pd.DataFrame(results_pythia,
                                  columns=["dataset", "num_sites","num_sites_del", "old_diff", "new_diff", "diff_change" ,"theshold_max", "min_support"])
 
-            if not os.path.isfile(os.path.join(os.pardir, "data/processed/final", "site_filter_min02.csv")):
-                df_py.to_csv(os.path.join(os.pardir, "data/processed/final", "site_filter_min02.csv"),
+            if not os.path.isfile(os.path.join(os.pardir, "data/processed/final", "site_filter_min01.csv")):
+                df_py.to_csv(os.path.join(os.pardir, "data/processed/final", "site_filter_min01.csv"),
                              index=False, header=True,
                              columns=["dataset","num_sites","num_sites_del", "old_diff", "new_diff", "diff_change","theshold_max", "min_support"])
             else:
-                df_py.to_csv(os.path.join(os.pardir, "data/processed/final", "site_filter_min02.csv"),
+                df_py.to_csv(os.path.join(os.pardir, "data/processed/final", "site_filter_min01.csv"),
                              index=False,
                              mode='a', header=False,
                              columns=["dataset", "num_sites", "num_sites_del", "old_diff", "new_diff", "diff_change","theshold_max", "min_support"])
