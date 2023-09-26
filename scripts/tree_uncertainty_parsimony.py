680b61709f7dd5733cedb204c4f8117eba3c41ce
juliusw <uimmv@student.kit.edu>
2023-09-26 09:45:04 +0200

    Changed

--- scripts/tree_uncertainty_parsimony.py
+++ scripts/tree_uncertainty_parsimony.py
@@ -53,7 +53,7 @@ for tree_filename in filenames:
         "raxml-ng",
         "--start",
         f"--model {model_path}",
-        "--tree pars{100}",
+        "--tree pars{1000}",
         f"--msa {msa_filepath}",
         "--redo",
         f"--prefix {output_prefix}"
