d001bd725fb7608e8cd39ba28f10220aef1ef5b4
juliusw <uimmv@student.kit.edu>
2023-09-30 19:33:17 +0200

    Changed

--- target/merge_datasets.py
+++ target/merge_datasets.py
@@ -313,7 +313,7 @@ def sample_rows(group):
     percentile = group["percentile"].iloc[0]
     print(percentile)
     if percentile <= 4.0:
-        max_sample_size = min(1000, len(group))
+        max_sample_size = 1500
     else:
         return group
     print(group.shape)
