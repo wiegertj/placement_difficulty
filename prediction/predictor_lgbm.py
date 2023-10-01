80336521bb02cea61b346de7904a11b5ae930ab2
juliusw <uimmv@student.kit.edu>
2023-10-01 09:25:00 +0200

    Changed

--- prediction/predictor_lgbm.py
+++ prediction/predictor_lgbm.py
@@ -113,12 +113,12 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=30, shapley_calc=True, targets=
         X_test = X_test.drop(axis=1, columns=['dataset', 'sampleId'])
 
     def objective(trial):
-        callbacks = [LightGBMPruningCallback(trial, 'l1')]
+        callbacks = [LightGBMPruningCallback(trial, 'quantile')]
 
         params = {
             'objective': 'regression',
             "n_estimators": trial.suggest_int('num_leaves', 200, 300),
-            'metric': 'l1',
+            'metric': 'quantile',
             'boosting_type': 'gbdt',
             'num_leaves': trial.suggest_int('num_leaves', 2, 100),
             'learning_rate': trial.suggest_loguniform('learning_rate', 0.001, 0.1),
@@ -284,4 +284,4 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=30, shapley_calc=True, targets=
         plt.savefig("lgbm-300.png")
 
 
-light_gbm_regressor(rfe=True, shapley_calc=True, targets=[])
+light_gbm_regressor(rfe=False, shapley_calc=False, targets=[])
