b89b9bd118c7e22797bd9893cb4b95e4d7103d0e
juliusw <uimmv@student.kit.edu>
2023-10-01 22:45:27 +0200

    Changed

--- prediction/predictor_lgbm_bs.py
+++ prediction/predictor_lgbm_bs.py
@@ -68,7 +68,7 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=20, shapley_calc=True):
         params = {
             'objective': 'regression',
             'metric': 'l1',
-            'num_boost_round': trial.suggest_int('num_leaves', 100, 300),
+            'num_boost_round': trial.suggest_int('num_boost_round', 100, 300),
             'boosting_type': 'gbdt',
             'num_leaves': trial.suggest_int('num_leaves', 2, 50),
             'learning_rate': trial.suggest_loguniform('learning_rate', 0.001, 0.1),
