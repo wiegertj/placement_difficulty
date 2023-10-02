40592dac896492bedc369311603218b9da6dfed5
juliusw <uimmv@student.kit.edu>
2023-10-02 14:08:43 +0200

    Changed

--- prediction/predictor_lgbm_bs.py
+++ prediction/predictor_lgbm_bs.py
@@ -84,7 +84,7 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=20, shapley_calc=True):
 
         val_scores = []
 
-        gkf = GroupKFold(n_splits=10)
+        gkf = GroupKFold(n_splits=20)
         for train_idx, val_idx in gkf.split(X_train.drop(axis=1, columns=['group']), y_train, groups=X_train["group"]):
             X_train_tmp, y_train_tmp = X_train.drop(axis=1, columns=['group']).iloc[train_idx], y_train.iloc[train_idx]
             X_val, y_val = X_train.drop(axis=1, columns=['group']).iloc[val_idx], y_train.iloc[val_idx]
@@ -102,7 +102,7 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=20, shapley_calc=True):
         return sum(val_scores) / len(val_scores)
 
     study = optuna.create_study(direction='minimize')
-    study.optimize(objective, n_trials=50)
+    study.optimize(objective, n_trials=100)
 
     best_params = study.best_params
     best_score = study.best_value
