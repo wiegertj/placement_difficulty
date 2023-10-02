bc1668208e872b00ce1a4ddb89f28712a9f477ca
juliusw <uimmv@student.kit.edu>
2023-10-02 09:40:30 +0200

    Changed

--- scripts/tree_uncertainty_parsimony.py
+++ scripts/tree_uncertainty_parsimony.py
@@ -80,28 +80,28 @@ for tree_filename in filenames:
                      "--redo",
                      f"--prefix {output_prefix}"]
     #result =  subprocess.run(" ".join(raxml_command), shell=True)
-    result = subprocess.run(" ".join(raxml_command), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+    #result = subprocess.run(" ".join(raxml_command), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
 
-    print("result")
-    print(result.stdout)
+    #print("result")
+    #print(result.stdout)
     # Check if the command was successful
-    if result.returncode == 0:
+    #if result.returncode == 0:
         # Extract numbers following "set: " using regular expressions
-        numbers = re.findall(r'set:\s+(-?[\d.]+)', result.stdout)
+      #  numbers = re.findall(r'set:\s+(-?[\d.]+)', result.stdout)
 
-        # Convert the extracted strings to integers or floats
-        numbers = [int(num) if num.isdigit() else float(num) for num in numbers]
+      #  # Convert the extracted strings to integers or floats
+     #   numbers = [int(num) if num.isdigit() else float(num) for num in numbers]
 
-        # Print the extracted numbers
-        print("Extracted numbers:", numbers)
-    else:
-        # Print an error message if the command failed
-        print("Command failed with the following error message:")
-        print(result.stderr)
-    try:
-        results.append((tree_filename.replace(".newick", ""), numbers[0], numbers[1], numbers[2]))
-    except IndexError:
-        print("number extraction failed ....")
+     #   # Print the extracted numbers
+     #   print("Extracted numbers:", numbers)
+    #else:
+      #  # Print an error message if the command failed
+      #  print("Command failed with the following error message:")
+      #  print(result.stderr)
+    #try:
+       # results.append((tree_filename.replace(".newick", ""), numbers[0], numbers[1], numbers[2]))
+    #except IndexError:
+        #print("number extraction failed ....")
 
-res_df = pd.DataFrame(results, columns=["dataset", "avg_rf", "avg_rel_rf", "no_top"])
-res_df.to_csv(os.path.join(os.pardir, "data/processed/features/bs_features/pars_top_features.csv"), index=False)
+#res_df = pd.DataFrame(results, columns=["dataset", "avg_rf", "avg_rel_rf", "no_top"])
+#res_df.to_csv(os.path.join(os.pardir, "data/processed/features/bs_features/pars_top_features.csv"), index=False)
