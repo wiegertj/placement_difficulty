3e0f475ca249464710c56d181258b9da01c886df
juliusw <uimmv@student.kit.edu>
2023-10-04 22:14:42 +0200

    Changed

--- prediction/predictor_quant_bs.py
+++ prediction/predictor_quant_bs.py
@@ -215,8 +215,8 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=20, shapley_calc=True):
     #####################################################################################################################
 
     X_test = X_test[["parsimony_support", "length", 'min_pars_supp_child_w', 'split_std_ratio_branch', 'group', 'avg_entropy_msa', 'mean_pars_supp_tree']]
-    X_test["sqrt_parsimony_support_tree"] = np.log(X_test['mean_pars_supp_tree'])
-    X_test["sqrt_parsimony_support"] = np.sqrt(X_test['parsimony_support'])
+    X_test["sqrt_parsimony_support_tree"] = pow(X_test['mean_pars_supp_tree'],2)
+    X_test["sqrt_parsimony_support"] = pow(X_test['parsimony_support'],2)
     X_test["sqrt3_parsimony_support_tree"] = pow(X_test['mean_pars_supp_tree'], 3)
     X_test["sqrt3_parsimony_support"] = pow(X_test['parsimony_support'], 3)
 
