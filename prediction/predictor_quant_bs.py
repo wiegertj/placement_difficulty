688e896ec1a391d8c64d919070f1edf93108ba10
juliusw <uimmv@student.kit.edu>
2023-10-04 10:27:24 +0200

    Changed

--- prediction/predictor_quant_bs.py
+++ prediction/predictor_quant_bs.py
@@ -111,6 +111,104 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=20, shapley_calc=True):
     if not rfe:
         X_train = X_train.drop(axis=1, columns=['dataset', 'branchId'])
         X_test = X_test.drop(axis=1, columns=['dataset', 'branchId'])
+    ######################################
+
+    def objective(trial):
+        #callbacks = [LightGBMPruningCallback(trial, 'l1')]
+
+        params = {
+            'objective': 'regression',
+            'metric': 'l1',
+            'num_iterations': trial.suggest_int('num_iterations', 100, 300),
+            'boosting_type': 'gbdt',
+            'num_leaves': trial.suggest_int('num_leaves', 2, 200),
+            'learning_rate': trial.suggest_uniform('learning_rate', 0.001, 0.1),
+            'min_child_samples': trial.suggest_int('min_child_samples', 1, 200),
+            #'feature_fraction': trial.suggest_uniform('feature_fraction', 0.5, 1.0),
+            'lambda_l1': trial.suggest_uniform('lambda_l1', 1e-5, 1.0),
+            'lambda_l2': trial.suggest_uniform('lambda_l2', 1e-5, 1.0),
+            'min_split_gain': trial.suggest_uniform('min_split_gain', 1e-5, 0.1),
+            'bagging_freq': 0,
+            #'bagging_fraction': trial.suggest_uniform('bagging_fraction', 0.5, 1.0)
+        }
+
+        val_scores = []
+
+        gkf = GroupKFold(n_splits=10)
+        for train_idx, val_idx in gkf.split(X_train.drop(axis=1, columns=['group']), y_train, groups=X_train["group"]):
+            X_train_tmp, y_train_tmp = X_train.drop(axis=1, columns=['group']).iloc[train_idx], y_train.iloc[train_idx]
+            X_val, y_val = X_train.drop(axis=1, columns=['group']).iloc[val_idx], y_train.iloc[val_idx]
+
+            train_data = lgb.Dataset(X_train_tmp, label=y_train_tmp)
+            val_data = lgb.Dataset(X_val, label=y_val, reference=train_data)
+            # KEIN VALIDSETS?
+            model = lgb.train(params, train_data)#, valid_sets=[val_data])
+            val_preds = model.predict(X_val)
+            #val_score = mean_squared_error(y_val, val_preds)
+            #val_score = math.sqrt(val_score)
+            val_score = mean_absolute_error(y_val, val_preds)
+            val_scores.append(val_score)
+
+        return np.median(val_scores)#sum(val_scores) / len(val_scores) #median?
+
+    study = optuna.create_study(direction='minimize')
+    study.optimize(objective, n_trials=50)
+
+    best_params = study.best_params
+    best_score = study.best_value
+
+    print(f"Best Params: {best_params}")
+    print(f"Best MAPE training: {best_score}")
+
+    train_data = lgb.Dataset(X_train.drop(axis=1, columns=["group"]), label=y_train)
+
+    final_model = lgb.train(best_params, train_data)
+
+    y_pred_median = final_model.predict(X_test.drop(axis=1, columns=["group"]))
+
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = math.sqrt(mse)
+    print(f"Root Mean Squared Error on test set: {rmse}")
+
+    r_squared = r2_score(y_test, y_pred)
+    print(f"R-squared on test set: {r_squared:.2f}")
+
+    mae = mean_absolute_error(y_test, y_pred)
+    print(f"MAE on test set: {mae:.2f}")
+
+    mape = median_absolute_error(y_test, y_pred)
+    print(f"MdAE on test set: {mape}")
+
+
+    residuals = y_test - y_pred
+
+    plt.scatter(y_pred, residuals)
+    plt.xlabel("Predicted Values")
+    plt.ylabel("Residuals")
+    plt.title("Residual Plot")
+    plt.axhline(y=0, color='r', linestyle='--')  # Add a horizontal line at y=0 for reference
+
+    # Save the plot as an image file (e.g., PNG)
+    plt.savefig("residual_plot.png")
+
+    feature_importance = final_model.feature_importance(importance_type='gain')
+
+    importance_df = pd.DataFrame(
+        {'Feature': X_train.drop(axis=1, columns=["group"]).columns, 'Importance': feature_importance})
+    importance_df = importance_df.sort_values(by='Importance', ascending=False)
+
+    scaler = MinMaxScaler()
+    importance_df['Importance'] = scaler.fit_transform(importance_df[['Importance']])
+    importance_df = importance_df.nlargest(30, 'Importance')
+
+    plt.figure(figsize=(10, 6))
+    plt.bar(importance_df['Feature'], importance_df['Importance'])
+    plt.xticks(rotation=90)
+    plt.xlabel('Features')
+    plt.ylabel('Importance')
+    plt.title('Feature Importances')
+    plt.tight_layout()
+
 
 
 
@@ -213,7 +311,7 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=20, shapley_calc=True):
         return sum(val_scores) / len(val_scores)
 
     study = optuna.create_study(direction='minimize')
-    study.optimize(objective_lower_bound, n_trials=50)
+    study.optimize(objective_higher_bound(), n_trials=50)
     solver = "highs" if sp_version >= parse_version("1.6.0") else "interior-point"
 
     best_params_hi = study.best_params
@@ -249,7 +347,7 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=20, shapley_calc=True):
     quant_loss_hi = quantile_loss(y_test, y_pred_hi, 0.9)
     print(f"Quantile Loss Holdout: {quant_loss_hi}" )
 
-    df_res = pd.DataFrame({'y_pred_lo': y_pred_lo, 'y_pred_hi': y_pred_hi})
+    df_res = pd.DataFrame({'y_pred_lo': y_pred_lo, 'y_pred_median': y_pred_median ,'y_pred_hi': y_pred_hi})
     df_res.to_csv(os.path.join(os.pardir, "data/processed/final", "pred_interval_new.csv"))
 
 light_gbm_regressor(rfe=False, shapley_calc=False)
