f4240767c4d8eb285d31e22b0d62cdfe92d46e61
juliusw <uimmv@student.kit.edu>
2023-10-04 22:39:30 +0200

    Changed

--- prediction/predictor_quant_bs.py
+++ prediction/predictor_quant_bs.py
@@ -60,6 +60,7 @@ def MBE(y_true, y_pred):
 def light_gbm_regressor(rfe=False, rfe_feature_n=20, shapley_calc=True):
     df = pd.read_csv(os.path.join(os.pardir, "data/processed/final", "bs_support.csv"))
     df_diff = pd.read_csv(os.path.join(os.pardir, "data/treebase_difficulty_new.csv"))
+    df_diff["name"] =     df_diff["name"].str.replace(".phy", "")
     df = df.merge(df_diff, left_on="dataset", right_on="name", how="inner")
     df.fillna(-1, inplace=True)
     df.replace([np.inf, -np.inf], -1, inplace=True)
