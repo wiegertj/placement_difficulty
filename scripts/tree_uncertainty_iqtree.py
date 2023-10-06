a3fe77ddca46101293e734bc861982c011b3a967
juliusw <uimmv@student.kit.edu>
2023-10-06 22:05:42 +0200

    Changed

--- scripts/tree_uncertainty_iqtree.py
+++ scripts/tree_uncertainty_iqtree.py
@@ -97,7 +97,7 @@ for tree_filename in filenames_filtered:
     ]
 
     #print("Boot")
-
+    print(" ".join(raxml_command))
     subprocess.run(" ".join(raxml_command), shell=True)
 
     print(f"Bootstrap analysis for {tree_filename} completed.")
