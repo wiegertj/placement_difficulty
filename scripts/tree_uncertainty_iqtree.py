00a2da43e8cb49c22e7633ea60763d20f241d33f
juliusw <uimmv@student.kit.edu>
2023-10-06 22:08:35 +0200

    Changed

--- scripts/tree_uncertainty_iqtree.py
+++ scripts/tree_uncertainty_iqtree.py
@@ -14,32 +14,9 @@ filenames = list(filenames)
 filenames = pd.read_csv(os.path.join(os.pardir, "data/loo_selection.csv"))["verbose_name"].str.replace(".phy", ".newick").values.tolist()
 
 
-filenames_filtered = []
+filenames_filtered = filenames
 duplicate_data = pd.read_csv(os.path.join(os.pardir, "data/treebase_difficulty_new.csv"))
 accepted = []
-for file in filenames:
-    try:
-        dupl = duplicate_data.loc[duplicate_data["name"] == file.replace(".newick", ".phy")].iloc[0]
-        dupl = dupl["no_duplicates"]
-    except IndexError:
-        print("File not found in placements")
-        continue
-    print(dupl)
-    print(file)
-
-    if dupl == 0:
-        print("removed")
-        accepted.append(file)
-    else:
-        filenames_filtered.append(file)
-    print("--------")
-accepted_df = pd.DataFrame(accepted, columns=["dataset"])
-accepted_df.to_csv(os.path.join(os.pardir, "data/bs_support_pred_selection.csv"), index=False)
-for file in filenames_filtered:
-    if not os.path.exists(os.path.join(os.pardir, "data/raw/reference_tree", file)):
-        print("Not found " + file)
-        filenames_filtered.remove(file)
-print(len(filenames_filtered))
 counter = 0
 for tree_filename in filenames_filtered:
     counter += 1
@@ -78,10 +55,9 @@ for tree_filename in filenames_filtered:
         print("Too large, skipped")
         continue
 
-    existing_csv_file = os.path.join(os.pardir, "data/bs_support_pred_selection.csv")
-    filtered_filenames_df_tmp = pd.DataFrame([tree_filename], columns=["dataset"])
-    filtered_filenames_df_tmp.to_csv(existing_csv_file, mode='a', header=False, index=False)
 
+   # filtered_filenames_df_tmp.to_csv(existing_csv_file, mode='a', header=False, index=False)
+#
     model_path = os.path.join(os.pardir, "data/processed/loo", tree_filename.replace(".newick", "") + "_msa_model.txt")
     output_prefix = tree_filename.split(".")[0] + "_1000"  # Using the filename as the prefix
 
