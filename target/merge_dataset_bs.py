2bd96416e4f9dd2eeafcf431a70abc0a51868c94
juliusw <uimmv@student.kit.edu>
2023-10-07 16:57:20 +0200

    Changed

--- target/merge_dataset_bs.py
+++ target/merge_dataset_bs.py
@@ -79,7 +79,7 @@ df_merged['irs_skw_ratio'].fillna(-1, inplace=True)
 df_merged['skw_clo_sim_ratio'].fillna(-1, inplace=True)
 
 step_size = 0.1
-max_samples_per_interval = 3500
+max_samples_per_interval = 3800
 
 # Initialize an empty DataFrame to store the sampled data
 sampled_data = pd.DataFrame()
