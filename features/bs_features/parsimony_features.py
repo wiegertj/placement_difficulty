2f536ef566a0d70602527e2465aa3d1b707aff34
juliusw <uimmv@student.kit.edu>
2023-10-08 17:19:20 +0200

    Changed

--- features/bs_features/parsimony_features.py
+++ features/bs_features/parsimony_features.py
@@ -18,60 +18,60 @@ for file in filenames:
     print(file)
 
     support_path = os.path.join(grandir, "scripts/") + file.replace(".newick", "") + "_parsimony_100.raxml.support"
-    support_path_low = os.path.join(grandir, "scripts/") + file.replace(".newick", "") + "_parsimony_100_low.raxml.support"
-    support_path_low1000 = os.path.join(grandir, "scripts/") + file.replace(".newick",
-                                                                        "") + "_parsimony_1000_low.raxml.support"
-    support_path_low500 = os.path.join(grandir, "scripts/") + file.replace(".newick",
-                                                                            "") + "_parsimony_500_low.raxml.support"
-    support_path_low2000 = os.path.join(grandir, "scripts/") + file.replace(".newick",
-                                                                        "") + "_parsimony_2000_low.raxml.support"
+    #support_path_low = os.path.join(grandir, "scripts/") + file.replace(".newick", "") + "_parsimony_100_low.raxml.support"
+    #support_path_low1000 = os.path.join(grandir, "scripts/") + file.replace(".newick",
+     #                                                                   "") + "_parsimony_1000_low.raxml.support"
+    #support_path_low500 = os.path.join(grandir, "scripts/") + file.replace(".newick",
+     #                                                                       "") + "_parsimony_500_low.raxml.support"
+    #support_path_low2000 = os.path.join(grandir, "scripts/") + file.replace(".newick",
+     #                                                                   "") + "_parsimony_2000_low.raxml.support"
     if not os.path.exists(support_path):
         print("Couldnt find support: " + support_path)
         continue
 
-    if not os.path.exists(support_path_low):
-        print("Couldnt find support low: " + support_path_low)
-        continue
+    #if not os.path.exists(support_path_low):
+     #   print("Couldnt find support low: " + support_path_low)
+      #  continue
 
-    with open(support_path_low, "r") as support_file_low:
-        tree_str_low = support_file_low.read()
-        tree_low = Tree(tree_str_low)
+    #with open(support_path_low, "r") as support_file_low:
+     #   tree_str_low = support_file_low.read()
+      #  tree_low = Tree(tree_str_low)
+#
+ #       branch_id_counter_low = 0
+#
+ #       for node_low in tree_low.traverse():
+  #          branch_id_counter_low += 1
+   #         node_low.__setattr__("name", branch_id_counter_low)
+#
+ #   with open(support_path_low1000, "r") as support_file_low1000:
+  #      tree_str_low1000 = support_file_low1000.read()
+   #     tree_low1000 = Tree(tree_str_low1000)
+#
+ #       branch_id_counter_low1000 = 0
+#
+ #       for node_low1000 in tree_low1000.traverse():
+  #          branch_id_counter_low1000 += 1
+   #         node_low1000.__setattr__("name", branch_id_counter_low1000)
+#
+ #   with open(support_path_low2000, "r") as support_file_low2000:
+  #      tree_str_low2000 = support_file_low2000.read()
+   #     tree_low2000 = Tree(tree_str_low2000)
+#
+ #       branch_id_counter_low2000 = 0
+#
+ #       for node_low2000 in tree_low2000.traverse():
+  #          branch_id_counter_low2000 += 1
+   #         node_low2000.__setattr__("name", branch_id_counter_low2000)
+#
+    #with open(support_path_low500, "r") as support_file_low500:
+ #       tree_str_low500 = support_file_low500.read()
+    #    tree_low500 = Tree(tree_str_low500)
+#
+       # branch_id_counter_low500 = 0
 
-        branch_id_counter_low = 0
-
-        for node_low in tree_low.traverse():
-            branch_id_counter_low += 1
-            node_low.__setattr__("name", branch_id_counter_low)
-
-    with open(support_path_low1000, "r") as support_file_low1000:
-        tree_str_low1000 = support_file_low1000.read()
-        tree_low1000 = Tree(tree_str_low1000)
-
-        branch_id_counter_low1000 = 0
-
-        for node_low1000 in tree_low1000.traverse():
-            branch_id_counter_low1000 += 1
-            node_low1000.__setattr__("name", branch_id_counter_low1000)
-
-    with open(support_path_low2000, "r") as support_file_low2000:
-        tree_str_low2000 = support_file_low2000.read()
-        tree_low2000 = Tree(tree_str_low2000)
-
-        branch_id_counter_low2000 = 0
-
-        for node_low2000 in tree_low2000.traverse():
-            branch_id_counter_low2000 += 1
-            node_low2000.__setattr__("name", branch_id_counter_low2000)
-
-    with open(support_path_low500, "r") as support_file_low500:
-        tree_str_low500 = support_file_low500.read()
-        tree_low500 = Tree(tree_str_low500)
-
-        branch_id_counter_low500 = 0
-
-        for node_low500 in tree_low500.traverse():
-            branch_id_counter_low500 += 1
-            node_low500.__setattr__("name", branch_id_counter_low500)
+        #for node_low500 in tree_low500.traverse():
+         #   branch_id_counter_low500 += 1
+          #  node_low500.__setattr__("name", branch_id_counter_low500)
 
 
     with open(support_path, "r") as support_file:
@@ -97,46 +97,46 @@ for file in filenames:
             branch_id_counter += 1
             node.__setattr__("name", branch_id_counter)
             if node.support is not None and not node.is_leaf():
-                node_low = tree_low.search_nodes(name=branch_id_counter)[0]
-                node_low_support = node_low.support
+               # node_low = tree_low.search_nodes(name=branch_id_counter)[0]
+                #node_low_support = node_low.support
 
 
-                diff_support_100 = node.support - node_low_support
+                #diff_support_100 = node.support - node_low_support
 
-                node_low1000 = tree_low1000.search_nodes(name=branch_id_counter)[0]
-                node_low2000 = tree_low2000.search_nodes(name=branch_id_counter)[0]
-                node_low_support2000 = node_low2000.support
+                #node_low1000 = tree_low1000.search_nodes(name=branch_id_counter)[0]
+                #node_low2000 = tree_low2000.search_nodes(name=branch_id_counter)[0]
+                #node_low_support2000 = node_low2000.support
 
-                node_low_support1000 = node_low1000.support
+                #node_low_support1000 = node_low1000.support
 
-                node_low500 = tree_low500.search_nodes(name=branch_id_counter)[0]
-                node_low_support500 = node_low500.support
+                #node_low500 = tree_low500.search_nodes(name=branch_id_counter)[0]
+                #node_low_support500 = node_low500.support
 
-                diff_support_500 = node.support - node_low_support500
+                #diff_support_500 = node.support - node_low_support500
 
-                all_supps.append(node.support)
-                all_supps.append(node_low_support)
-                all_supps.append(node_low_support1000)
-                all_supps.append(node_low_support500)
-                all_diff_supps.append(node_low_support2000)
-
-
-
+                #all_supps.append(node.support)
+                #all_supps.append(node_low_support)
+                #all_supps.append(node_low_support1000)
+                #all_supps.append(node_low_support500)
+                #all_diff_supps.append(node_low_support2000)
 
 
 
 
 
 
-                diff_support_1000 = node.support - node_low_support1000
-                diff_support_2000 = node.support - node_low_support2000
 
-                diff_support_500 = node.support - node_low_support500
 
-                all_diff_supps.append(diff_support_100)
-                all_diff_supps.append(diff_support_500)
-                all_diff_supps.append(diff_support_1000)
-                all_diff_supps.append(diff_support_2000)
+
+                #diff_support_1000 = node.support - node_low_support1000
+                #diff_support_2000 = node.support - node_low_support2000
+
+                #diff_support_500 = node.support - node_low_support500
+
+                #all_diff_supps.append(diff_support_100)
+                #all_diff_supps.append(diff_support_500)
+                #all_diff_supps.append(diff_support_1000)
+                #all_diff_supps.append(diff_support_2000)
 
                 mean_all_supps = statistics.mean(all_supps)
                 std_all_supps = np.std(all_supps)
@@ -236,9 +236,7 @@ for file in filenames:
                                 mean_pars_supp_parents, std_pars_supp_parents, skw_pars_supp_parents,
                                 min_pars_supp_parents_w,
                                 max_pars_supp_parents_w,
-                                mean_pars_supp_parents_w, std_pars_supp_parents_w, skw_pars_supp_parents_w, min_pars_supp_tree, max_pars_supp_tree, std_pars_supp_tree, skw_pars_supp_tree, mean_pars_supp_tree,
-                                diff_support_100 /100, node_low_support/100, diff_support_1000/100, node_low_support1000 / 100, diff_support_500/100, node_low_support500 / 100,
-                                diff_support_2000 / 100, node_low_support2000,mean_all_supps, std_all_supps, mean_all_diff_supps, std_all_diff_supps, max_all_supps, min_all_supps
+                                mean_pars_supp_parents_w, std_pars_supp_parents_w, skw_pars_supp_parents_w, min_pars_supp_tree, max_pars_supp_tree, std_pars_supp_tree, skw_pars_supp_tree, mean_pars_supp_tree,mean_all_supps, std_all_supps, mean_all_diff_supps, std_all_diff_supps, max_all_supps, min_all_supps
                                 ))
 
 
