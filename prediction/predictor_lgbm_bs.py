8b4eb2d930ceda17525ed3683d064bbcbb9026e1
juliusw <uimmv@student.kit.edu>
2023-10-08 18:47:11 +0200

    Changed

--- prediction/predictor_lgbm_bs.py
+++ prediction/predictor_lgbm_bs.py
@@ -72,9 +72,9 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=20, shapley_calc=True):
     model = RandomForestRegressor(n_jobs=-1, n_estimators=250, max_depth=10, min_samples_split=20,
                                   min_samples_leaf=10)
     rfe = RFE(estimator=model, n_features_to_select=10)  # Adjust the number of features as needed
-    rfe.fit(X_train.drop(axis=1, columns=['dataset', 'sampleId', 'group']), y_train)
+    rfe.fit(X_train.drop(axis=1, columns=['dataset', 'branchId', 'group']), y_train)
     print(rfe.support_)
-    selected_features = X_train.drop(axis=1, columns=['dataset', 'sampleId', 'group']).columns[rfe.support_]
+    selected_features = X_train.drop(axis=1, columns=['dataset', 'branchId', 'group']).columns[rfe.support_]
     selected_features = selected_features.append(pd.Index(['group']))
 
     print("Selected features for RFE: ")
