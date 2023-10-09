1021adfee92ca8819e14a8386142feeb9b6aa4b9
juliusw <uimmv@student.kit.edu>
2023-10-09 17:53:15 +0200

    Changed

--- prediction/predict_consensus_distance.py
+++ prediction/predict_consensus_distance.py
@@ -87,11 +87,11 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=20, shapley_calc=True):
     train = df[~df['group'].isin(sample_dfs)]
 
     X_train = train.drop(axis=1, columns=target)
-    X_train = X_train.drop(axis=1, columns=["quartet"])
+    #X_train = X_train.drop(axis=1, columns=["quartet"])
     y_train = train[target]
 
     X_test = test.drop(axis=1, columns=target)
-    X_test = X_test.drop(axis=1, columns=["quartet"])
+    #X_test = X_test.drop(axis=1, columns=["quartet"])
 
     y_test = test[target]
 
