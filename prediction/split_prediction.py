006a30bea3ff0e2f5a4e68fdc2e7eb2e95156527
juliusw <uimmv@student.kit.edu>
2023-10-12 16:28:24 +0200

    Changed

--- prediction/split_prediction.py
+++ prediction/split_prediction.py
@@ -40,8 +40,8 @@ def MBE(y_true, y_pred):
 
 
 def light_gbm_regressor(rfe=False, rfe_feature_n=20, shapley_calc=True):
-    df_msa = pd.read_csv(os.path.join(os.pardir, "data/processed/features", "msa_features.csv"))
-    df_target = pd.read_csv(os.path.join(os.pardir, "data/processed/final", "split_prediction.csv"))
+    df_msa = pd.read_csv(os.path.join(os.pardir, "data/processed/features", "msa_features.csv"), usecols=lambda column: column != 'Unnamed: 0')
+    df_target = pd.read_csv(os.path.join(os.pardir, "data/processed/final", "split_prediction.csv"), usecols=lambda column: column != 'Unnamed: 0')
     df = df_msa.merge(df_target, on=["dataset"], how="inner")
     df["group"] = df['dataset'].astype('category').cat.codes.tolist()
     print(df.columns)
