fc3a2f1e1f959b421904e7c15da3bf8a9f79849e
juliusw <uimmv@student.kit.edu>
2023-10-12 19:48:16 +0200

    Changed

--- features/split_features/parsimony_split_target.py
+++ features/split_features/parsimony_split_target.py
@@ -7,14 +7,17 @@
 # ---------------------------------------------
 # parsimony_branchId | branch_support_parsimony_consensus_tree_noboot | branch_support_parsimony_consensus_tree_boot |... MSA Features/Pars_topo_features ... | inML
 # => classifier with probability
-
+import statistics
 import subprocess
 
 import ete3
 import pandas as pd
 import os
 from ete3 import Tree
+import numpy as np
 from Bio import SeqIO
+from scipy.stats import skew
+
 
 def get_bipartition(node):
 
@@ -118,7 +121,96 @@ for file in filenames:
                                 second_match = True
                             if second_match and first_match:
                                     node_in_ml_tree = 1
-                results.append((dataset, node.name, node.support, node_in_ml_tree, level))
+
+                childs_inner = [node_child for node_child in node.traverse() if not node_child.is_leaf()]
+                parents_inner = node.get_ancestors()
+                supports_childs = []
+                weighted_supports_childs = []
+                for child in childs_inner:
+                    supports_childs.append(child.support)
+                    weighted_supports_childs.append(child.support * child.get_distance(phylo_tree, topology_only=True))
+
+                supports_parents = []
+                weighted_supports_parents = []
+                for parent in parents_inner:
+                    supports_parents.append(parent.support)
+                    weighted_supports_parents.append(parent.support * parent.dist)
+
+                if len(supports_childs) >= 1:
+                    min_pars_supp_child = min(supports_childs)
+                    max_pars_supp_child = max(supports_childs)
+                    mean_pars_supp_child = statistics.mean(supports_childs)
+                else:
+                    min_pars_supp_child = -1
+                    max_pars_supp_child = -1
+                    mean_pars_supp_child = -1
+                    std_pars_supp_child = -1
+                    skw_pars_supp_child = -1
+
+                if len(supports_childs) > 1:
+                    std_pars_supp_child = np.std(supports_childs)
+                    skw_pars_supp_child = skew(supports_childs)
+
+                ###
+                if len(weighted_supports_childs) >= 1:
+                    min_pars_supp_child_w = min(weighted_supports_childs)
+                    max_pars_supp_child_w = max(weighted_supports_childs)
+                    mean_pars_supp_child_w = statistics.mean(weighted_supports_childs)
+                else:
+                    min_pars_supp_child_w = -1
+                    max_pars_supp_child_w = -1
+                    mean_pars_supp_child_w = -1
+                    std_pars_supp_child_w = -1
+                    skw_pars_supp_child_w = -1
+
+                if len(weighted_supports_childs) > 1:
+                    std_pars_supp_child_w = np.std(weighted_supports_childs)
+                    skw_pars_supp_child_w = skew(weighted_supports_childs)
+
+                ############
+
+                if len(supports_parents) >= 1:
+                    min_pars_supp_parents = min(supports_parents)
+                    max_pars_supp_parents = max(supports_parents)
+                    mean_pars_supp_parents = statistics.mean(supports_parents)
+                else:
+                    min_pars_supp_parents = -1
+                    max_pars_supp_parents = -1
+                    mean_pars_supp_parents = -1
+                    std_pars_supp_parents = -1
+                    skw_pars_supp_parents = -1
+
+                if len(supports_parents) > 1:
+                    std_pars_supp_parents = np.std(supports_parents)
+                    skw_pars_supp_parents = skew(supports_parents)
+
+                ###
+
+                if len(weighted_supports_parents) >= 1:
+                    min_pars_supp_parents_w = min(weighted_supports_parents)
+                    max_pars_supp_parents_w = max(weighted_supports_parents)
+                    mean_pars_supp_parents_w = statistics.mean(weighted_supports_parents)
+                else:
+                    min_pars_supp_parents_w = -1
+                    max_pars_supp_parents_w = -1
+                    mean_pars_supp_parents_w = -1
+                    std_pars_supp_parents_w = -1
+                    skw_pars_supp_parents_w = -1
+
+                if len(weighted_supports_parents) > 1:
+                    std_pars_supp_parents_w = np.std(weighted_supports_parents)
+                    skw_pars_supp_parents_w = skew(weighted_supports_parents)
+
+                results.append((dataset, node.name, node.support, node_in_ml_tree, level,
+                                min_pars_supp_parents_w, max_pars_supp_parents_w, mean_pars_supp_parents_w,
+                                std_pars_supp_parents_w, skw_pars_supp_parents_w,
+                                min_pars_supp_parents, max_pars_supp_parents, mean_pars_supp_parents,
+                                std_pars_supp_parents, skw_pars_supp_parents,
+                                min_pars_supp_child_w, max_pars_supp_child_w, mean_pars_supp_child_w,
+                                std_pars_supp_child_w, skw_pars_supp_child_w,
+                                min_pars_supp_child, max_pars_supp_child, mean_pars_supp_child,
+                                std_pars_supp_child, skw_pars_supp_child
+                                ))
     else:
         print("Not found support")
         not_counter += 1
