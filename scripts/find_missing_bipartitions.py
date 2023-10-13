d03dac1538802b10c01b297b7ee23905f59e9e53
juliusw <uimmv@student.kit.edu>
2023-10-13 11:24:47 +0200

    Changed

---
+++ scripts/find_missing_bipartitions.py
@@ -0,0 +1,82 @@
+from dendropy import Tree, TaxonNamespace
+import itertools
+import pandas as pd
+import os
+df = pd.read_csv(os.path.join(os.pardir, "data/processed/final/split_prediction.csv"))
+test_set = df["dataset"].unique().tolist()[0]
+
+trees_pars = os.path.join(os.pardir, "scripts",
+                          test_set + "_parsimony_1000_nomodel.raxml.startTree")
+
+def get_bipartition(node):
+    if not node.is_leaf():
+        try:
+            left_children = sorted([leaf.name for leaf in node.children[0].iter_leaves()])
+            right_children = sorted([leaf.name for leaf in node.children[1].iter_leaves()])
+            bipartition = (left_children, right_children)
+            return bipartition
+        except IndexError:
+            return None
+    return None
+# Read the true and false bipartitions
+df_test = df[df["dataset"] == test_set]
+
+consensus_path = os.path.join(os.pardir, "features/split_features",
+                                  test_set + "_consensus1000nomodel_.raxml.consensusTreeMRE")
+
+
+true_bipartitions = []
+false_bipartitions = []
+
+with open(consensus_path, "r") as cons:
+    tree_str = cons.read()
+    phylo_tree = Tree(tree_str)
+    branch_id_counter_ref = 0
+    for node in phylo_tree.traverse():
+        branch_id_counter_ref += 1
+        if not node.is_leaf():
+            node.__setattr__("name", branch_id_counter_ref)
+            matching_row = df[df['branchIdpars'] == node.name]
+            inML = matching_row["inML"]
+            if inML == 1:
+                true_bipartitions.append(get_bipartition(node))
+            else:
+                false_bipartitions.append(get_bipartition(node))
+
+
+# Define your threshold for compatibility (less than 50%)
+compatibility_threshold = 0.5
+
+# Create a TaxonNamespace for your taxa
+taxon_namespace = TaxonNamespace()
+
+# Read newick trees from the file and process them
+with open(trees_pars, "r") as tree_file:
+    for line in tree_file:
+        # Parse the newick tree
+        tree = Tree.get(data=line, schema="newick", taxon_namespace=taxon_namespace)
+
+        # Extract bipartitions from the tree
+        bipartitions = set()
+        for node in tree:
+            bipartitions.add(frozenset(node.leafset()))
+
+        # Check compatibility and add to the true list if criteria met
+        add_to_true_list = True
+        for true_bp in true_bipartitions:
+            if not all(set(bp).isdisjoint(true_bp) for bp in bipartitions):
+                add_to_true_list = False
+                break
+        for false_bp in false_bipartitions:
+            overlap_count = sum(1 for bp in bipartitions if not set(bp).isdisjoint(false_bp))
+            if overlap_count / len(bipartitions) >= compatibility_threshold:
+                add_to_true_list = False
+                break
+        if add_to_true_list:
+            true_bipartitions.extend(bipartitions)
+
+        # Check if you have a complete tree
+        if len(true_bipartitions) == len(taxon_namespace) - 1:
+            break
+
+# Now your true_bipartitions list contains bipartitions for your complete tree
