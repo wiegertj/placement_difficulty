c58c57369605ec6dc995dc4df3156ccf7e39e683
juliusw <uimmv@student.kit.edu>
2023-10-13 15:07:14 +0200

    Changed

--- scripts/find_missing_bipartitions.py
+++ scripts/find_missing_bipartitions.py
@@ -1,6 +1,8 @@
+import subprocess
 import sys
 from math import inf
 
+import ete3
 from dendropy import Tree, TaxonNamespace
 import itertools
 from ete3 import Tree
@@ -74,6 +76,59 @@ with open(trees_pars, "r") as tree_file:
                score -= 1
         if score > score_max:
             score_max = score
-            best_tree = line
+            best_tree = tree
     print("Best Score: " + str(score_max))
-    print("Best Tree: " + str(line))
+    print("Best Tree: " + str(best_tree))
+
+    best_tree.write(outfile=test_set + "best_pars_tree.newick", format=1)
+
+
+
+
+
+
+    original_path = os.path.join(os.pardir, "data/raw/reference_tree",
+                                 test_set)
+
+    command = ["/home/wiegerjs/tqDist-1.0.2/bin/quartet_dist", "-v", os.path.abspath(test_set + "best_pars_tree.newick"),
+               os.path.abspath(original_path)]
+    try:
+        command_string = " ".join(command)
+        print(command_string)
+        output = subprocess.check_output(command, stderr=subprocess.STDOUT, text=True)
+        lines = output.strip().split('\n')
+        values = lines[0].split()
+        quartet_distance = float(values[3])
+    except:
+        print("quartet went wrong")
+
+    with open(original_path, 'r') as original_file:
+
+        original_newick_tree = original_file.read()
+        original_tree = ete3.Tree(original_newick_tree)
+
+        with open(consensus_path, 'r') as consensus_file:
+            consensus_newick_tree = consensus_file.read()
+            consensus_tree = ete3.Tree(consensus_newick_tree)
+
+        results_distance = original_tree.compare(consensus_tree, unrooted=True)
+
+        nrf_distance = results_distance["norm_rf"]
+
+        print(nrf_distance)
+        print(quartet_distance)
+
+    results = [(test_set, nrf_distance, quartet_distance)]
+    df = pd.DataFrame(results, columns=["dataset", "nrf", "quartet"])
+    if not os.path.isfile(os.path.join(os.pardir, "data/processed/features/bs_features",
+                                       "cons_comp_target_best_pars.csv")):
+        df.to_csv(os.path.join(os.path.join(os.pardir, "data/processed/features/bs_features",
+                                            "cons_comp_target_best_pars.csv")), index=False)
+    else:
+        df.to_csv(os.path.join(os.pardir, "data/processed/features/bs_features",
+                               "cons_comp_target_best_pars.csv"),
+                  index=False,
+                  mode='a', header=False)
+
+
+
