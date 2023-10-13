c5fe3f8a0ef7659dc174e9b815b88aac8b905654
juliusw <uimmv@student.kit.edu>
2023-10-13 10:18:40 +0200

    Changed

--- features/split_features/parsimony_split_target.py
+++ features/split_features/parsimony_split_target.py
@@ -320,156 +320,7 @@ for file in filenames:
 
 
 
-                # Create Split MSA
-                list_a = []
-                list_a_dist_branch = []
-                list_a_dist_topo = []
-                list_b = []
-                list_b_dist_branch = []
-                list_b_dist_topo = []
-                for leaf in phylo_tree.get_leaves():
-                    if leaf in node.get_leaves():
-                        list_a.append(leaf.name)
-                        list_a_dist_branch.append(leaf.get_distance(target=phylo_tree.get_tree_root()))
-                        list_a_dist_topo.append(
-                            leaf.get_distance(topology_only=True, target=phylo_tree.get_tree_root()))
-                    else:
-                        list_b.append(leaf.name)
-                        list_b_dist_branch.append(leaf.get_distance(target=phylo_tree.get_tree_root()))
-                        list_b_dist_topo.append(
-                            leaf.get_distance(topology_only=True, target=phylo_tree.get_tree_root()))
 
-                if len(list_a_dist_branch) == 0 or len(list_b_dist_branch) == 0:
-                    continue
-
-                split_len_a_b = min(len(list_b), len(list_a)) / max(len(list_b), len(list_a))
-                split_min_dist_branch_a = min(list_a_dist_branch)
-                split_max_dist_branch_a = max(list_a_dist_branch)
-                split_mean_dist_branch_a = statistics.mean(list_a_dist_branch)
-                split_std_dist_branch_a = np.std(list_a_dist_branch)
-                split_skew_dist_branch_a = skew(list_a_dist_branch)
-
-                split_min_dist_branch_b = min(list_b_dist_branch)
-                split_max_dist_branch_b = max(list_b_dist_branch)
-                split_mean_dist_branch_b = statistics.mean(list_b_dist_branch)
-                split_std_dist_branch_b = np.std(list_b_dist_branch)
-                split_skew_dist_branch_b = skew(list_b_dist_branch)
-
-                if max(split_min_dist_branch_a, split_min_dist_branch_b) != 0:
-
-                    split_min_ratio_branch = min(split_min_dist_branch_a, split_min_dist_branch_b) / max(
-                        split_min_dist_branch_a, split_min_dist_branch_b)
-                    split_max_ratio_branch = min(split_max_dist_branch_a, split_max_dist_branch_b) / max(
-                        split_max_dist_branch_a, split_max_dist_branch_b)
-                    split_std_ratio_branch = min(split_std_dist_branch_a, split_std_dist_branch_b) / max(
-                        split_std_dist_branch_a, split_std_dist_branch_b)
-                    split_mean_ratio_branch = min(split_mean_dist_branch_a, split_mean_dist_branch_b) / max(
-                        split_mean_dist_branch_a, split_mean_dist_branch_b)
-                else:
-                    split_min_ratio_branch = 0
-                    split_max_ratio_branch = 0
-                    split_std_ratio_branch = 0
-                    split_mean_ratio_branch = 0
-
-                try:
-                    split_skw_ratio_branch = min(split_skew_dist_branch_a, split_skew_dist_branch_b) / max(
-                        split_skew_dist_branch_a, split_skew_dist_branch_b)
-                except ZeroDivisionError:
-                    split_skw_ratio_branch = 0
-
-                split_min_dist_topo_a = min(list_a_dist_topo)
-                split_max_dist_topo_a = max(list_a_dist_topo)
-                split_mean_dist_topo_a = statistics.mean(list_a_dist_topo)
-                split_std_dist_topo_a = np.std(list_a_dist_topo)
-                split_skew_dist_topo_a = skew(list_a_dist_topo)
-
-                split_min_dist_topo_b = min(list_b_dist_topo)
-                split_max_dist_topo_b = max(list_b_dist_topo)
-                split_mean_dist_topo_b = statistics.mean(list_b_dist_topo)
-                split_std_dist_topo_b = np.std(list_b_dist_topo)
-                split_skew_dist_topo_b = skew(list_b_dist_topo)
-
-                split_min_ratio_topo = min(split_min_dist_topo_a, split_min_dist_topo_b) / max(
-                    split_min_dist_topo_a, split_min_dist_topo_b)
-                split_max_ratio_topo = min(split_max_dist_topo_a, split_max_dist_topo_b) / max(
-                    split_max_dist_topo_a, split_max_dist_topo_b)
-                split_std_ratio_topo = min(split_std_dist_topo_a, split_std_dist_topo_b) / max(
-                    split_std_dist_topo_a, split_std_dist_topo_b)
-                split_mean_ratio_topo = min(split_mean_dist_topo_a, split_mean_dist_topo_b) / max(
-                    split_mean_dist_topo_a, split_mean_dist_topo_b)
-                try:
-                    split_skw_ratio_topo = min(split_skew_dist_topo_a, split_skew_dist_topo_b) / max(
-                        split_skew_dist_topo_a, split_skew_dist_topo_b)
-                except ZeroDivisionError:
-                    split_skw_ratio_topo = 0
-
-                alignment = AlignIO.read(msa_path, 'fasta')
-                alignment_a = MultipleSeqAlignment([])
-                alignment_b = MultipleSeqAlignment([])
-                for record in alignment:
-                    if record.id in list_a:
-                        alignment_a.append(record)
-                    elif record.id in list_b:
-                        alignment_b.append(record)
-
-                freqs_b = []
-                freqs_a = []
-
-                entropy_differences = []
-
-                for i in range(len(alignment_a[0])):
-                    column_a = alignment_a[:, i]
-
-                    column_b = alignment_b[:, i]
-
-                    combined_values = column_a + column_b
-                    all_keys = set(combined_values)
-
-                    counter_a = Counter({key: 0 for key in all_keys})
-                    counter_b = Counter({key: 0 for key in all_keys})
-
-                    counter_a.update(column_a)
-                    counter_b.update(column_b)
-
-                    sorted_keys = sorted(all_keys)
-
-                    counter_a = Counter({key: counter_a[key] for key in sorted_keys})
-                    counter_b = Counter({key: counter_b[key] for key in sorted_keys})
-
-                    freqs_a.append(counter_a)
-                    freqs_b.append(counter_b)
-
-                for site_freq_a, site_freq_b in zip(freqs_a, freqs_b):
-                    total_count_a = sum(site_freq_a.values())
-                    total_count_b = sum(site_freq_b.values())
-                    try:
-                        normalized_freq_a = {k: v / total_count_a for
-                                             k, v in site_freq_a.items()}
-                    except ZeroDivisionError:
-                        normalized_freq_a = {k: 0 for
-                                             k, v in site_freq_a.items()}
-                    try:
-                        normalized_freq_b = {k: v / total_count_b for
-                                             k, v in site_freq_b.items()}
-                    except:
-                        normalized_freq_b = {k: 0 for
-                                             k, v in site_freq_b.items()}
-
-                    site_freq_a_array = np.array(list(normalized_freq_a.values()))
-
-                    site_freq_b_array = np.array(list(normalized_freq_b.values()))
-
-                    entropy_a = entropy(site_freq_a_array)
-                    entropy_b = entropy(site_freq_b_array)
-
-                    entropy_difference = abs(entropy_a - entropy_b)
-                    entropy_differences.append(entropy_difference)
-
-                split_min_entropy_diff = min(entropy_differences)
-                split_max_entropy_diff = max(entropy_differences)
-                split_std_entropy_diff = np.std(entropy_differences)
-                split_mean_entropy_diff = statistics.mean(entropy_differences)
-                split_skw_entropy_diff = skew(entropy_differences)
 
 
 
@@ -492,14 +343,8 @@ for file in filenames:
                                 min_pars_supp_child, max_pars_supp_child, mean_pars_supp_child,
                                 std_pars_supp_child, skw_pars_supp_child,
                                 irs_mean_right, irs_mean_left, irs_min_left, irs_min_right,
-                                irs_max_left, irs_max_right, irs_std_left, irs_std_right, irs_skw_left, irs_skw_right,
-                                split_len_a_b, split_min_entropy_diff, split_max_entropy_diff, split_std_entropy_diff,
-                                split_mean_entropy_diff, split_skw_entropy_diff, split_min_ratio_topo,
-                                split_max_ratio_topo,
-                                split_std_ratio_topo, split_mean_ratio_topo, split_skw_ratio_topo,
-                                split_min_ratio_branch,
-                                split_max_ratio_branch, split_max_ratio_branch, split_mean_ratio_branch,
-                                split_skw_ratio_branch, split_std_ratio_branch
+                                irs_max_left, irs_max_right, irs_std_left, irs_std_right, irs_skw_left, irs_skw_right
+
                                 ))
     else:
         print("Not found support")
@@ -518,10 +363,6 @@ result_df = pd.DataFrame(results, columns=["dataset", "parsBranchId", "pars_supp
                                            "std_pars_supp_child", "skw_pars_supp_child",
 "irs_mean_right", "irs_mean_left", "irs_min_left", "irs_min_right",
                                 "irs_max_left", "irs_max_right", "irs_std_left", "irs_std_right", "irs_skw_left", "irs_skw_right",
-"split_len_a_b", "split_min_entropy_diff", "split_max_entropy_diff", "split_std_entropy_diff",
-                          "split_mean_entropy_diff", "split_skw_entropy_diff", "split_min_ratio_topo", "split_max_ratio_topo",
-                          "split_std_ratio_topo", "split_mean_ratio_topo", "split_skw_ratio_topo", "split_min_ratio_branch",
-                          "split_max_ratio_branch", "split_max_ratio_branch", "split_mean_ratio_branch",
-                          "split_skw_ratio_branch", "split_std_ratio_branch"
+
                                            ])
 result_df.to_csv(os.path.join(grandir, "data/processed/final/split_prediction.csv"))
