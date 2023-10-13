07a1ba729e61ed0cbe3fb5612aecd5e393e55466
juliusw <uimmv@student.kit.edu>
2023-10-13 17:42:08 +0200

    Changed

--- prediction/split_prediction.py
+++ prediction/split_prediction.py
@@ -42,7 +42,7 @@ def MBE(y_true, y_pred):
 
 def light_gbm_regressor(rfe=False, rfe_feature_n=20, shapley_calc=True):
     df_msa = pd.read_csv(os.path.join(os.pardir, "data/processed/features", "msa_features.csv"), usecols=lambda column: column != 'Unnamed: 0')
-    df_target = pd.read_csv(os.path.join(os.pardir, "data/processed/final", "split_prediction.csv"), usecols=lambda column: column != 'Unnamed: 0')
+    df_target = pd.read_csv(os.path.join(os.pardir, "data/processed/final", "split_prediction_10000.csv"), usecols=lambda column: column != 'Unnamed: 0')
     print("unique datasets: ")
     print(len(df_target["dataset"].unique()))
     df = df_msa.merge(df_target, on=["dataset"], how="inner")
@@ -169,7 +169,7 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=20, shapley_calc=True):
         return np.mean(val_scores)  # sum(val_scores) / len(val_scores) #median?
 
     study = optuna.create_study(direction='maximize')
-    study.optimize(objective, n_trials=10)
+    study.optimize(objective, n_trials=50)
 
     best_params = study.best_params
     best_score = study.best_value
