3383a91a0dcfe148357a94e8b4ff9f2838459f0c
juliusw <uimmv@student.kit.edu>
2023-10-14 22:45:22 +0200

    Changed

--- target/raxml_old_bs.py
+++ target/raxml_old_bs.py
@@ -75,7 +75,7 @@ for tree_filename in filenames_filtered:
 
     raxml_command = [
         "raxmlHPC",
-        f"-m {model_path}",
+        f"-m GTRGAMMA",
         f"-s {msa_filepath}",
         f"-# {1000}",
         "-p 12345",
