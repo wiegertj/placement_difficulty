b4cfc66f47e34aa62463d9c0d29c89a89754acfa
juliusw <uimmv@student.kit.edu>
2023-10-16 16:43:48 +0200

    Changed

--- data_analysis/iqtree_vs_raxml_vs_EBG.py
+++ data_analysis/iqtree_vs_raxml_vs_EBG.py
@@ -5,10 +5,15 @@ file_path = "/hits/fast/cme/wiegerjs/placement_difficulty/prediction"
 all_dataframes = []
 
 all_dataframes = []
-
+counter = 0
 for root, dirs, files in os.walk(file_path):
+    # Skip the "ebg_tmp" directory and its contents
+    if "ebg_tmp" in dirs:
+        dirs.remove("ebg_tmp")
     for filename in files:
         if filename.endswith('.csv'):
+            counter += 1
+            print(counter)
             file_pathname = os.path.join(root, filename)
             df = pd.read_csv(file_pathname)
             all_dataframes.append(df)
