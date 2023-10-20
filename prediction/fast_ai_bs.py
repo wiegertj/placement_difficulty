9c7e1218b98a7a5d3b315e9103a6663f3c708b2a
juliusw <uimmv@student.kit.edu>
2023-10-20 23:36:22 +0200

    init

--- prediction/fast_ai_bs.py
+++ prediction/fast_ai_bs.py
@@ -1,59 +1,186 @@
-import os
+import copy
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from fastai.tabular.all import *
-from sklearn.metrics import mean_absolute_error, mean_squared_error
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import tqdm
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import fetch_california_housing
 
+df = pd.read_csv(os.path.join(os.pardir, "data/processed/final", "bs_support.csv"))
 
-def light_gbm_regressor():
-    # Load your dataset (modify the path as needed)
-    df = pd.read_csv(os.path.join(os.pardir, "data/processed/final", "bs_support.csv"))
+df = df[["dataset", "branchId", "support", "parsimony_boot_support",
+         "parsimony_support",
+         "avg_subst_freq",
+         "length_relative",
+         "length",
+         "avg_rel_rf_boot",
+         "max_subst_freq",
+         "skw_pars_bootsupp_tree",
+         "cv_subst_freq",
+         "bl_ratio",
+         "max_pars_bootsupp_child_w",
+         "sk_subst_freq",
+         "mean_pars_bootsupp_parents",
+         "max_pars_supp_child_w",
+         "std_pars_bootsupp_parents",
+         "min_pars_supp_child",
+         "min_pars_supp_child_w",
+         "rel_num_children",
+         "mean_pars_supp_child_w",
+         "std_pars_bootsupp_child",
+         "mean_clo_sim_ratio",
+         "min_pars_bootsupp_child_w"]]
 
-    loo_selection = pd.read_csv(os.path.join(os.pardir, "data/loo_selection.csv"))
-    loo_selection["dataset"] = loo_selection["verbose_name"].str.replace(".phy", "")
-    loo_selection = loo_selection[:200]
-    filenames = loo_selection["dataset"].values.tolist()
-    df.fillna(-1, inplace=True)
-    test = df[df['dataset'].isin(filenames)]
-    print(test)
-    y_test = test["support"].values
-    #test = test.drop(columns=["support"], axis=1)
-    train = df[~df['dataset'].isin(filenames)]
-    print(train)
-    test = test.drop(columns=["dataset", "branchId"], axis=1)
-    train = train.drop(columns=["dataset", "branchId"], axis=1)
+column_name_mapping = {
+    "parsimony_boot_support": "parsimony_bootstrap_support",
+    "parsimony_support": "parsimony_support",
+    "avg_subst_freq": "mean_substitution_frequency",
+    "length_relative": "norm_branch_length",
+    "length": "branch_length",
+    "avg_rel_rf_boot": "mean_norm_rf_distance",
+    "max_subst_freq": "max_substitution_frequency",
+    "skw_pars_bootsupp_tree": "skewness_bootstrap_pars_support_tree",
+    "cv_subst_freq": "cv_substitution_frequency",
+    "bl_ratio": "branch_length_ratio_split",
+    "max_pars_bootsupp_child_w": "max_pars_bootstrap_support_children_w",
+    "sk_subst_freq": "skw_substitution_frequency",
+    "mean_pars_bootsupp_parents": "mean_pars_bootstrap_support_parents",
+    "max_pars_supp_child_w": "max_pars_support_children_weighted",
+    "std_pars_bootsupp_parents": "std_pars_bootstrap_support_parents",
+    "min_pars_supp_child": "min_pars_support_children",
+    "min_pars_supp_child_w": "min_pars_support_children_weighted",
+    "rel_num_children": "number_children_relative",
+    "mean_pars_supp_child_w": "mean_pars_support_children_weighted",
+    "std_pars_bootsupp_child": "std_pars_bootstrap_support_children",
+    "mean_clo_sim_ratio": "mean_closeness_centrality_ratio",
+    "min_pars_bootsupp_child_w": "min_pars_bootstrap_support_children_w"
+}
 
-    # Create a TabularPandas object
-    procs = [Categorify, FillMissing, Normalize]
-    cont_names = [col for col in train.columns if col != 'support']
+# Rename the columns in the DataFrame
+df = df.rename(columns=column_name_mapping)
 
-    data = TabularPandas(train, procs=procs, cat_names=[], cont_names=cont_names, y_names="support")
+# df_diff = pd.read_csv(os.path.join(os.pardir, "data/treebase_difficulty_new.csv"))
+# df_diff["name"] = df_diff["name"].str.replace(".phy", "")
+# df = df.merge(df_diff, left_on="dataset", right_on="name", how="inner")
+# df.drop(columns=["datatype", "name"], axis=1, inplace=True)
 
-    # Create DataLoaders
-    dls = data.dataloaders(bs=64)  # Adjust batch size as needed
+df.fillna(-1, inplace=True)
+df.replace([np.inf, -np.inf], -1, inplace=True)
+print("Median Support: ")
+print(df["support"].median())
+df.columns = df.columns.str.replace(':', '_')
 
-    # Create a regression learner with MAE as the loss function
-    learn = tabular_learner(dls, layers=[200, 100], metrics=[mean_absolute_error])
+print(df.columns)
+print(df.shape)
 
-    # Train the model
-    learn.fit_one_cycle(5)  # Modify the number of epochs as needed
+df["group"] = df['dataset'].astype('category').cat.codes.tolist()
 
-    procs = [Categorify, FillMissing, Normalize]
-    cont_names = [col for col in train.columns if col != 'support']
+target = "support"
 
-    test_data = data.new(test)
+loo_selection = pd.read_csv(os.path.join(os.pardir, "data/loo_selection.csv"))
+loo_selection["dataset"] = loo_selection["verbose_name"].str.replace(".phy", "")
+loo_selection = loo_selection[:200]
+filenames = loo_selection["dataset"].values.tolist()
 
-    test_dataloader = learn.dls.test_dl(test_data)
+test = df[df['dataset'].isin(filenames)]
+train = df[~df['dataset'].isin(filenames)]
 
-    # Use the 'get_preds' method on the learner 'learn' to get predictions for the test data
-    dl = learn.dls.test_dl(test)
-    raw_test_preds = learn.get_preds(dl=dl)
-    preds = raw_test_preds[0].numpy().T[0]
+# print(test.shape)
+# print(train.shape)
 
-    # Calculate MAE on the test set
-    mae = mean_absolute_error(y_test, preds)
-    print(f"Mean Absolute Error on test set: {mae}")
+#####
 
+# sample_dfs = random.sample(df["group"].unique().tolist(), int(len(df["group"].unique().tolist()) * 0.2))
+# test = df[df['group'].isin(sample_dfs)]
+# train = df[~df['group'].isin(sample_dfs)]
 
-# Call the function to run the Fastai Tabular Learner
-light_gbm_regressor()
+X_train = train.drop(axis=1, columns=target)
+y_train = train[target]
+
+X_test = test.drop(axis=1, columns=target)
+y_test = test[target]
+
+# print(test.shape)
+# print(train.shape)
+
+#####
+
+# sample_dfs = random.sample(df["group"].unique().tolist(), int(len(df["group"].unique().tolist()) * 0.2))
+# test = df[df['group'].isin(sample_dfs)]
+# train = df[~df['group'].isin(sample_dfs)]
+
+X_train = train.drop(axis=1, columns=target)
+y_train = train[target]
+
+X_test = test.drop(axis=1, columns=target)
+y_test = test[target]
+
+# Convert to 2D PyTorch tensors
+X_train = torch.tensor(X_train, dtype=torch.float32)
+y_train = torch.tensor(y_train, dtype=torch.float32).reshape(-1, 1)
+X_test = torch.tensor(X_test, dtype=torch.float32)
+y_test = torch.tensor(y_test, dtype=torch.float32).reshape(-1, 1)
+
+# Define the model
+model = nn.Sequential(
+    nn.Linear(8, 24),
+    nn.ReLU(),
+    nn.Linear(24, 12),
+    nn.ReLU(),
+    nn.Linear(12, 6),
+    nn.ReLU(),
+    nn.Linear(6, 1)
+)
+
+# loss function and optimizer
+loss_fn = nn.MSELoss()  # mean square error
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
+
+n_epochs = 100   # number of epochs to run
+batch_size = 10  # size of each batch
+batch_start = torch.arange(0, len(X_train), batch_size)
+
+# Hold the best model
+best_mse = np.inf   # init to infinity
+best_weights = None
+history = []
+
+for epoch in range(n_epochs):
+    model.train()
+    with tqdm.tqdm(batch_start, unit="batch", mininterval=0, disable=True) as bar:
+        bar.set_description(f"Epoch {epoch}")
+        for start in bar:
+            # take a batch
+            X_batch = X_train[start:start+batch_size]
+            y_batch = y_train[start:start+batch_size]
+            # forward pass
+            y_pred = model(X_batch)
+            loss = loss_fn(y_pred, y_batch)
+            # backward pass
+            optimizer.zero_grad()
+            loss.backward()
+            # update weights
+            optimizer.step()
+            # print progress
+            bar.set_postfix(mse=float(loss))
+    # evaluate accuracy at end of each epoch
+    model.eval()
+    y_pred = model(X_test)
+    mse = loss_fn(y_pred, y_test)
+    mse = float(mse)
+    history.append(mse)
+    if mse < best_mse:
+        best_mse = mse
+        best_weights = copy.deepcopy(model.state_dict())
+
+# restore model and return best accuracy
+model.load_state_dict(best_weights)
+print("MSE: %.2f" % best_mse)
+print("RMSE: %.2f" % np.sqrt(best_mse))
+plt.plot(history)
+plt.show()
