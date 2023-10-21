8da3021ad12680aea89961e75a9002c9fc7aaa93
juliusw <uimmv@student.kit.edu>
2023-10-21 18:55:31 +0200

    init

--- prediction/predictor_lgbm_bs_quant.py
+++ prediction/predictor_lgbm_bs_quant.py
@@ -431,7 +431,7 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=20, shapley_calc=True):
         return sum(val_scores) / len(val_scores)
 
     study = optuna.create_study(direction='minimize')
-    study.optimize(objective_upper_bound, n_trials=100)
+    study.optimize(objective_upper_bound, n_trials=200)
 
     best_params_upper_bound = study.best_params
     best_params_upper_bound["objective"] = "quantile"
