f0059716dd6db72e6f16921d3482c52313b9d47f
juliusw <uimmv@student.kit.edu>
2023-10-21 19:16:56 +0200

    init

--- prediction/predictor_lgbm_bs_quant.py
+++ prediction/predictor_lgbm_bs_quant.py
@@ -408,7 +408,7 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=20, shapley_calc=True):
             'lambda_l1': trial.suggest_uniform('lambda_l1', 1e-5, 1.0),
             'lambda_l2': trial.suggest_uniform('lambda_l2', 1e-5, 1.0),
             'min_split_gain': trial.suggest_uniform('min_split_gain', 1e-5, 0.3),
-            'bagging_freq': trial.suggest_uniform('bagging_freq', 0.1, 1.0),
+            'bagging_freq': trial.suggest_int('bagging_freq', 3, 10),
             "verbosity": -1
         }
 
