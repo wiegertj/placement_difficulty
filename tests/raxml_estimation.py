40219fbcc846cc28bc48811587e43322b93db9ab
juliusw <uimmv@student.kit.edu>
2023-10-23 10:20:49 +0200

    Init

--- tests/raxml_estimation.py
+++ tests/raxml_estimation.py
@@ -8,7 +8,7 @@ list_foldernames = pd.read_csv(os.pardir + "/data/folder_names_raxml.csv")["fold
 path = os.pardir + "/data/raxml_data"
 counter=0
 # Loop over each subdirectory (folder) within the specified path
-for folder_name in os.listdir(path):
+for folder_name in list_foldernames:
     folder_path = os.path.join(path, folder_name)
     print(folder_name)
     counter += 1
@@ -28,11 +28,35 @@ for folder_name in os.listdir(path):
                 f"--model GTR+G",
                 f"--msa {abs_msa_file_path}",
                 "--redo",
-                "--tree pars{50}, rand{50}",
+                "--tree pars{50} rand{50}",
                 f"--prefix {folder_name}"
                 ]
 
                 subprocess.run(" ".join(raxml_command), shell=True)
 
-                model_path = ""
+                model_path = path + "/" + folder_name + ".raxml.bestModel"
+                tree_path = path + "/" + folder_name + ".raxml.bestTree"
 
+                raxml_command = [
+                    "raxml-ng",
+                    "--bootstrap",
+                    f"--model {model_path}",
+                    f"--bs-trees {1000}",
+                    f"--msa {abs_msa_file_path}",
+                    "--redo",
+                    f"--prefix {folder_name}"
+                ]
+
+                subprocess.run(" ".join(raxml_command), shell=True)
+
+                print(f"Bootstrap analysis for {folder_name} completed.")
+                boot_path = path + "/" + folder_name + ".raxml.bootstraps"
+                raxml_command = [
+                    "raxml-ng",
+                "--support",
+                f"--tree {tree_path}",
+                f"--bs-trees {boot_path}",
+                "--redo",
+                f"--prefix {folder_name}"]
+
+                subprocess.run(" ".join(raxml_command), shell=True)
