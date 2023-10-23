a4bc4cd6ff26e495969911becc582dd2befcdd57
juliusw <uimmv@student.kit.edu>
2023-10-23 11:11:47 +0200

    Init

--- tests/raxml_estimation.py
+++ tests/raxml_estimation.py
@@ -9,13 +9,14 @@ path = os.pardir + "/data/raxml_data"
 counter=0
 # Loop over each subdirectory (folder) within the specified path
 for folder_name in list_foldernames:
-    folder_path = os.path.join(path, str(folder_name))
+    folder_path = os.path.abspath(os.path.join(path, str(folder_name)))
     print(folder_path)
     counter += 1
     print(counter)
     if os.path.isdir(folder_path):
         msa_file_path = os.path.join(folder_path, 'msa.fasta')
         abs_msa_file_path = os.path.abspath(msa_file_path)
+        print(abs_msa_file_path)
         if os.path.exists(abs_msa_file_path):
             with open(msa_file_path, 'r') as msa_file:
                 os.chdir(folder_path)
