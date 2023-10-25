d2093c6ed2370548ff95f40b9035a5099d313a9b
juliusw <uimmv@student.kit.edu>
2023-10-25 23:34:35 +0200

    test

--- prediction/predictor_quant_bs.py
+++ prediction/predictor_quant_bs.py
@@ -110,32 +110,38 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=20, shapley_calc=True):
         "min_pars_bootsupp_child_w": "min_pars_bootstrap_support_children_w"
     }
 
+
+
+
+
+
     # Rename the columns in the DataFrame
     df = df.rename(columns=column_name_mapping)
 
     df_raxml = pd.read_csv(os.path.join(os.pardir, "data/processed/final", "df_pred.csv"), usecols=lambda column: column != 'Unnamed: 0' and column != 'Unnamed: 0_x' and column != 'Unnamed: 0_y')
     df_raxml_filtered = df_raxml[["dataset", "branchId", "support", "parsimony_boot_support",
-             "parsimony_support",
-             "avg_subst_freq",
-             "length_relative",
-             "length",
-             "avg_rel_rf_boot",
-             "max_subst_freq",
-             "skw_pars_bootsupp_tree",
-             "cv_subst_freq",
-             "bl_ratio",
-             "max_pars_bootsupp_child_w",
-             "sk_subst_freq",
-             "mean_pars_bootsupp_parents",
-             "max_pars_supp_child_w",
-             "std_pars_bootsupp_parents",
-             "min_pars_supp_child",
-             "min_pars_supp_child_w",
-             "rel_num_children",
-             "mean_pars_supp_child_w",
-             "std_pars_bootsupp_child",
-             "mean_clo_sim_ratio",
-             "min_pars_bootsupp_child_w"]]
+                                   "parsimony_support",
+                                   "avg_subst_freq",
+                                   "length_relative",
+                                   "length",
+                                   "avg_rel_rf_boot",
+                                   "max_subst_freq",
+                                   "skw_pars_bootsupp_tree",
+                                   "cv_subst_freq",
+                                   "bl_ratio",
+                                   "max_pars_bootsupp_child_w",
+                                   "sk_subst_freq",
+                                   "mean_pars_bootsupp_parents",
+                                   "max_pars_supp_child_w",
+                                   "std_pars_bootsupp_parents",
+                                   "min_pars_supp_child",
+                                   "min_pars_supp_child_w",
+                                   "rel_num_children",
+                                   "mean_pars_supp_child_w",
+                                   "std_pars_bootsupp_child",
+                                   "mean_clo_sim_ratio",
+                                   "min_pars_bootsupp_child_w"
+                                   ]]
     print(df.shape)
     df = pd.concat([df, df_raxml_filtered])
     print(df.shape)
