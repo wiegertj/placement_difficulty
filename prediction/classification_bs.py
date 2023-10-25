be9ff2172d34b50e31ad25ce2f16ca997d2c43ca
juliusw <uimmv@student.kit.edu>
2023-10-25 13:14:40 +0200

    test

--- prediction/classification_bs.py
+++ prediction/classification_bs.py
@@ -132,8 +132,8 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=10, shapley_calc=True):
 
     X_test_ = X_test
     if not rfe:
-        X_train = X_train.drop(axis=1, columns=['dataset', 'branchId'])
-        X_test = X_test.drop(axis=1, columns=['dataset', 'branchId'])
+        X_train = X_train.drop(axis=1, columns=['dataset'])
+        X_test = X_test.drop(axis=1, columns=['dataset'])
 
     def objective(trial):
         # callbacks = [LightGBMPruningCallback(trial, 'l1')]
