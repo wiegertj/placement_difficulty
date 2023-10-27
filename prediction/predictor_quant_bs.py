4f73b3c57a1d661b1e53742afd10d6f8844f5910
juliusw <uimmv@student.kit.edu>
2023-10-27 15:09:00 +0200

    test

--- prediction/predictor_quant_bs.py
+++ prediction/predictor_quant_bs.py
@@ -264,7 +264,7 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=20, shapley_calc=True):
         return sum(val_scores) / len(val_scores)
 
     study = optuna.create_study(direction='minimize')
-    study.optimize(objective_median, n_trials=50)
+    study.optimize(objective_median, n_trials=1)
 
     best_params = study.best_params
     best_params["objective"] = "quantile"
@@ -282,7 +282,7 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=20, shapley_calc=True):
 
     final_model = lgb.train(best_params, train_data)
 
-    model_path = os.path.join(os.pardir, "data/processed/final", "mean_model90_test_nonuni.pkl")
+    model_path = os.path.join(os.pardir, "data/processed/final", "median_model_final.pkl")
     with open(model_path, 'wb') as file:
         pickle.dump(final_model, file)
 
@@ -336,158 +336,141 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=20, shapley_calc=True):
     #####################################################################################################################
     # X_test = X_test[["parsimony_support", "length", 'min_pars_supp_child_w', 'split_std_ratio_branch', 'group']]
     # X_train = X_train[["parsimony_support", "length", 'min_pars_supp_child_w', 'split_std_ratio_branch', 'group']]
-    X_test = X_test[["parsimony_bootstrap_support", "parsimony_support", "mean_substitution_frequency",
-                     "branch_length", "norm_branch_length", "max_substitution_frequency", 'group']]
-    X_train = X_train[["parsimony_bootstrap_support", "parsimony_support", "mean_substitution_frequency",
-                       "branch_length", "norm_branch_length", "max_substitution_frequency", 'group']]
 
     def objective_lower_bound(trial):
         # callbacks = [LightGBMPruningCallback(trial, 'l1')]
 
         params = {
-
-            'alpha': trial.suggest_float('alpha', 0.0001, 0.1),
-
+            'objective': 'quantile',
+            'metric': 'quantile',
+            'alpha': 0.05,
+            'num_iterations': trial.suggest_int('num_iterations', 50, 300),
+            'boosting_type': 'gbdt',
+            'num_leaves': trial.suggest_int('num_leaves', 2, 200),
+            'learning_rate': trial.suggest_uniform('learning_rate', 0.001, 0.9),
+            'min_child_samples': trial.suggest_int('min_child_samples', 1, 200),
+            # 'feature_fraction': trial.suggest_uniform('feature_fraction', 0.5, 1.0),
+            'lambda_l1': trial.suggest_uniform('lambda_l1', 1e-5, 1.0),
+            'lambda_l2': trial.suggest_uniform('lambda_l2', 1e-5, 1.0),
+            'min_split_gain': trial.suggest_uniform('min_split_gain', 1e-5, 0.3),
+            'bagging_freq': 0,
+            "verbosity": -1
         }
 
         val_scores = []
 
-        gkf = GroupKFold(n_splits=3)
+        gkf = GroupKFold(n_splits=5)
         for train_idx, val_idx in gkf.split(X_train.drop(axis=1, columns=['group']), y_train, groups=X_train["group"]):
             X_train_tmp, y_train_tmp = X_train.drop(axis=1, columns=['group']).iloc[train_idx], y_train.iloc[train_idx]
             X_val, y_val = X_train.drop(axis=1, columns=['group']).iloc[val_idx], y_train.iloc[val_idx]
-
             train_data = lgb.Dataset(X_train_tmp, label=y_train_tmp)
-            val_data = lgb.Dataset(X_val, label=y_val)  # , reference=train_data)
+            val_data = lgb.Dataset(X_val, label=y_val, reference=train_data)
             # KEIN VALIDSETS?
-            solver = "highs" if sp_version >= parse_version("1.6.0") else "interior-point"
-
-            model = QuantileRegressor(**params, quantile=0.05, solver=solver)
-
-            model = model.fit(X_train_tmp, y_train_tmp)
+            model = lgb.train(params, train_data)  # , valid_sets=[val_data])
             val_preds = model.predict(X_val)
             val_score = quantile_loss(y_val, val_preds, 0.05)
-            print("score: " + str(val_score))
-
             val_scores.append(val_score)
 
         return sum(val_scores) / len(val_scores)
 
-    solver = "highs" if sp_version >= parse_version("1.6.0") else "interior-point"
-
     study = optuna.create_study(direction='minimize')
-    study.optimize(objective_lower_bound, n_trials=6)
+    study.optimize(objective_lower_bound, n_trials=1)
 
-    best_params_lo = study.best_params
-    best_score_lo = study.best_value
+    best_params_lower_bound = study.best_params
+    best_params_lower_bound["objective"] = "quantile"
+    best_params_lower_bound["metric"] = "quantile"
+    best_params_lower_bound["boosting_type"] = "gbdt"
+    best_params_lower_bound["bagging_freq"] = 0
+    best_params_lower_bound["alpha"] = 0.05
+    best_params_lower_bound["verbosity"] = -1
+    best_score_lower_bound = study.best_value
 
-    print(f"Best Params: {best_params_lo}")
-    print(f"Best MAPE training: {best_score_lo}")
+    print(f"Best Params: {best_params_lower_bound}")
+    print(f"Best Quantile Loss: {best_score_lower_bound}")
 
-    model_lo = QuantileRegressor(**best_params_lo, quantile=0.05, solver=solver).fit(
-        X_train.drop(axis=1, columns=["group"]), y_train)
+    train_data = lgb.Dataset(X_train.drop(axis=1, columns=["group"]), label=y_train)
 
-    model_path = os.path.join(os.pardir, "data/processed/final", "low_model90_test.pkl")
+    final_model_lower_bound = lgb.train(best_params_lower_bound, train_data)
+
+    model_path = os.path.join(os.pardir, "data/processed/final", "low_model_final.pkl")
     with open(model_path, 'wb') as file:
-        pickle.dump(model_lo, file)
+        pickle.dump(final_model_lower_bound, file)
 
-    y_pred_lo = model_lo.predict(X_test.drop(axis=1, columns=["group"]))
-    quant_loss_lo = quantile_loss(y_test, y_pred_lo, 0.05)
-    print(f"Quantile Loss Holdout: {quant_loss_lo}")
-    mse = mean_squared_error(y_test, y_pred_lo)
-    rmse = math.sqrt(mse)
-    print(f"Root Mean Squared Error on test set: {rmse}")
-
-    mae = mean_absolute_error(y_test, y_pred_lo)
-    print(f"MAE on test set: {mae:.2f}")
-
-    mape = mean_absolute_percentage_error(y_test, y_pred_lo)
-    print(f"MAPE on test set: {mape}")
-
-    mdae = median_absolute_error(y_test, y_pred_lo)
-    print(f"MDAE on test set: {mdae}")
-
-    mbe = MBE(y_test, y_pred_lo)
-    print(f"MBE on test set: {mbe}")
+    y_pred_lower = final_model_lower_bound.predict(X_test.drop(axis=1, columns=["group"]))
+    print("Quantile Loss on Holdout: " + str(quantile_loss(y_test, y_pred_lower, 0.05)))
 
     ######################################################################################################
+
     def objective_higher_bound(trial):
         # callbacks = [LightGBMPruningCallback(trial, 'l1')]
 
         params = {
-
-            'alpha': trial.suggest_float('alpha', 0.0001, 0.1),
-
+            'objective': 'quantile',
+            'metric': 'quantile',
+            'alpha': 0.95,
+            'num_iterations': trial.suggest_int('num_iterations', 50, 300),
+            'boosting_type': 'gbdt',
+            'num_leaves': trial.suggest_int('num_leaves', 2, 200),
+            'learning_rate': trial.suggest_uniform('learning_rate', 0.001, 0.9),
+            'min_child_samples': trial.suggest_int('min_child_samples', 1, 200),
+            # 'feature_fraction': trial.suggest_uniform('feature_fraction', 0.5, 1.0),
+            'lambda_l1': trial.suggest_uniform('lambda_l1', 1e-5, 1.0),
+            'lambda_l2': trial.suggest_uniform('lambda_l2', 1e-5, 1.0),
+            'min_split_gain': trial.suggest_uniform('min_split_gain', 1e-5, 0.3),
+            'bagging_freq': 0,
+            "verbosity": -1
         }
 
         val_scores = []
 
-        gkf = GroupKFold(n_splits=3)
+        gkf = GroupKFold(n_splits=5)
         for train_idx, val_idx in gkf.split(X_train.drop(axis=1, columns=['group']), y_train, groups=X_train["group"]):
             X_train_tmp, y_train_tmp = X_train.drop(axis=1, columns=['group']).iloc[train_idx], y_train.iloc[train_idx]
             X_val, y_val = X_train.drop(axis=1, columns=['group']).iloc[val_idx], y_train.iloc[val_idx]
-
             train_data = lgb.Dataset(X_train_tmp, label=y_train_tmp)
-            val_data = lgb.Dataset(X_val, label=y_val)  # , reference=train_data)
+            val_data = lgb.Dataset(X_val, label=y_val, reference=train_data)
             # KEIN VALIDSETS?
-            model = QuantileRegressor(**params, quantile=0.85, solver=solver).fit(X_train_tmp, y_train_tmp)
+            model = lgb.train(params, train_data)  # , valid_sets=[val_data])
             val_preds = model.predict(X_val)
-            val_score = quantile_loss(y_val, val_preds, 0.85)
-            print("score: " + str(val_score))
-
+            val_score = quantile_loss(y_val, val_preds, 0.95)
             val_scores.append(val_score)
 
         return sum(val_scores) / len(val_scores)
 
     study = optuna.create_study(direction='minimize')
-    study.optimize(objective_higher_bound, n_trials=6)
-    solver = "highs" if sp_version >= parse_version("1.6.0") else "interior-point"
+    study.optimize(objective_higher_bound, n_trials=1)
 
-    best_params_hi = study.best_params
-    best_score_hi = study.best_value
+    best_params_higher_bound = study.best_params
+    best_params_higher_bound["objective"] = "quantile"
+    best_params_higher_bound["metric"] = "quantile"
+    best_params_higher_bound["boosting_type"] = "gbdt"
+    best_params_higher_bound["bagging_freq"] = 0
+    best_params_higher_bound["alpha"] = 0.95
+    best_params_higher_bound["verbosity"] = -1
+    best_score_higher_bound = study.best_value
 
-    print(f"Best Params: {best_params_hi}")
-    print(f"Best Q training: {best_score_hi}")
+    print(f"Best Params: {best_params_higher_bound}")
+    print(f"Best Quantile Loss: {best_score_higher_bound}")
 
-    model_hi = QuantileRegressor(**best_params_hi, quantile=0.85, solver=solver).fit(
-        X_train.drop(axis=1, columns=["group"]), y_train)
+    train_data = lgb.Dataset(X_train.drop(axis=1, columns=["group"]), label=y_train)
 
-    model_path = os.path.join(os.pardir, "data/processed/final", "high_model90_test.pkl")
+    final_model_higher_bound = lgb.train(best_params_higher_bound, train_data)
+
+    model_path = os.path.join(os.pardir, "data/processed/final", "high_model_final.pkl")
     with open(model_path, 'wb') as file:
-        pickle.dump(model_hi, file)
+        pickle.dump(final_model_higher_bound, file)
 
-    y_pred_hi = model_hi.predict(X_test.drop(axis=1, columns=["group"]))
-
-    mse = mean_squared_error(y_test, y_pred_hi)
-    rmse = math.sqrt(mse)
-    print(f"Root Mean Squared Error on test set: {rmse}")
-
-    mae = mean_absolute_error(y_test, y_pred_hi)
-    print(f"MAE on test set: {mae:.2f}")
-
-    mape = mean_absolute_percentage_error(y_test, y_pred_hi)
-    print(f"MAPE on test set: {mape}")
-
-    mdae = median_absolute_error(y_test, y_pred_hi)
-    print(f"MDAE on test set: {mdae}")
-
-    mbe = MBE(y_test, y_pred_hi)
-    print(f"MBE on test set: {mbe}")
-
-    quant_loss_lo = quantile_loss(y_test, y_pred_lo, 0.05)
-    print(f"Quantile Loss Holdout: {quant_loss_lo}")
-
-    quant_loss_hi = quantile_loss(y_test, y_pred_hi, 0.85)
-    print(f"Quantile Loss Holdout: {quant_loss_hi}")
+    y_pred_higher = final_model_higher_bound.predict(X_test.drop(axis=1, columns=["group"]))
+    print("Quantile Loss on Holdout: " + str(quantile_loss(y_test, y_pred_lower, 0.95)))
 
     X_test_["prediction_median"] = y_pred_median
-    X_test_["prediction_low"] = y_pred_lo
-    X_test_["prediction_hi"] = y_pred_hi
+    X_test_["prediction_low"] = y_pred_lower
+    X_test_["prediction_hi"] = y_pred_higher
     X_test_["support"] = y_test
     X_test_["pred_error"] = y_test - y_pred_median
-    X_test_["pi_width"] = y_pred_hi - y_pred_lo
+    X_test_["pi_width"] = y_pred_higher - y_pred_lower
 
-    X_test_.to_csv(os.path.join(os.pardir, "data/processed/final", "pred_interval_90_final.csv"))
+    X_test_.to_csv(os.path.join(os.pardir, "data/processed/final", "test_final.csv"))
 
 
 light_gbm_regressor(rfe=False, shapley_calc=False)
