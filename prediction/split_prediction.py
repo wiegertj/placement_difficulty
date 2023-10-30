c8841aca7259c69544e472a76d931c308e37b1ad
juliusw <uimmv@student.kit.edu>
2023-10-30 14:35:36 +0100

    New models

--- prediction/split_prediction.py
+++ prediction/split_prediction.py
@@ -57,7 +57,7 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=10, shapley_calc=True):
     print("Baseline")
     accuracy_best = -10
     for cutoff in range(1, 99):
-        val_preds_binary_baseline = (df["min_pars_supp_child"] > cutoff).astype(int)
+        val_preds_binary_baseline = (df["min_pars_supp_child"] >= cutoff).astype(int)
 
         accuracy = accuracy_score(df["inML"], val_preds_binary_baseline)
         if accuracy >= accuracy_best:
