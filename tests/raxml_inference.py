ad1a873c825b4802ce3722992b6c08518f280af2
juliusw <uimmv@student.kit.edu>
2023-11-02 10:28:14 +0100

    New models

--- tests/raxml_inference.py
+++ tests/raxml_inference.py
@@ -14,6 +14,7 @@ loo_selection_aa = pd.read_csv(os.path.join(os.pardir, "data/loo_selection_aa_te
 loo_selection["dataset"] = loo_selection["verbose_name"].str.replace(".phy", "")
 loo_selection = loo_selection[:180]
 loo_selection = pd.concat([loo_selection, loo_selection_aa])
+loo_selection = loo_selection[:240]
 print(loo_selection)
 print(loo_selection.shape)
 
@@ -36,9 +37,8 @@ for file in filenames:
         "--search",
         f"--msa {msa_filepath}",
         "--model GTR+G",
-        "--tree pars{50},rand{50}",
         "--redo",
-        "--threads 1"
+        "--threads auto{60}"
 
     ]
 
@@ -55,12 +55,12 @@ for file in filenames:
     time_dat = pd.DataFrame(data_lost)
 
     if not os.path.isfile(os.path.join(os.pardir, "data/processed/features/bs_features",
-                                       "inference_times_standard_nomt.csv")):
+                                       "inference_times_standard_20.csv")):
         time_dat.to_csv(os.path.join(os.path.join(os.pardir, "data/processed/features/bs_features",
-                                                  "inference_times_standard_nomt.csv")), index=False)
+                                                  "inference_times_standard_20.csv")), index=False)
     else:
         time_dat.to_csv(os.path.join(os.pardir, "data/processed/features/bs_features",
-                                     "inference_times_standard_nomt.csv"),
+                                     "inference_times_standard_20.csv"),
                         index=False,
                         mode='a', header=False)
 
