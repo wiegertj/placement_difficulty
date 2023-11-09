73e63aeb98de6874dfa6bbace313583ebeca5d1f
juliusw <uimmv@student.kit.edu>
2023-11-09 18:04:41 +0100

    Init

--- scripts/dimitri.py
+++ scripts/dimitri.py
@@ -63,7 +63,7 @@ counter = 0
 for root, dirs, files in os.walk(search_directory):
     if file_name in files:
         counter += 1
-        if counter <= 481:
+        if counter <= 601:
             continue
         print(counter)
         #print(root)
