51f6a313a02b9f683a98eb108764974d589d3b16
juliusw <uimmv@student.kit.edu>
2023-11-16 12:50:33 +0100

    get raw data

--- tests/copy_raw_data.py
+++ tests/copy_raw_data.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 import pandas as pd
-loo_selection = pd.read_csv(os.path.join(os.pardir, "data/processed/final/bs_supports.csv"))
+loo_selection = pd.read_csv(os.path.join(os.pardir, "data/processed/final/bs_support.csv"))
 loo_selection = loo_selection["dataset"].values.tolist()
 loo_selection_aa = pd.read_csv("/hits/fast/cme/wiegerjs/placement_difficulty/data/loo_selection_aa_test.csv")
 loo_selection_aa = loo_selection_aa["name"].str.replace(".phy").values.tolist()
