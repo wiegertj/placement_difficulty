a0512aebc9af568340debeb1103b7e0cc1c1744b
juliusw <uimmv@student.kit.edu>
2023-12-04 22:28:49 +0100

    init

--- prediction/prediction_site_diff.py
+++ prediction/prediction_site_diff.py
@@ -28,7 +28,7 @@ def light_gbm_regressor(cutoff, rfe=False, rfe_feature_n=10, shapley_calc=True):
 
     df["class"] = 0
     df.loc[df['diff_diff'] > 0, 'class'] = 1
-    df.loc[df['diff_diff'] < 0, 'class'] = 1
+    df.loc[df['diff_diff'] < 0, 'class'] = 0
     print(df["class"].value_counts())
 
     df["group"] = df['dataset'].astype('category').cat.codes.tolist()
@@ -86,13 +86,13 @@ def light_gbm_regressor(cutoff, rfe=False, rfe_feature_n=10, shapley_calc=True):
             'num_leaves': trial.suggest_int('num_leaves', 2, 300),
             'learning_rate': trial.suggest_uniform('learning_rate', 0.001, 0.3),
             'min_child_samples': trial.suggest_int('min_child_samples', 1, 200),
-            # 'feature_fraction': trial.suggest_uniform('feature_fraction', 0.5, 1.0),
+            'feature_fraction': trial.suggest_uniform('feature_fraction', 0.0, 1.0),
             'lambda_l1': trial.suggest_uniform('lambda_l1', 1e-5, 1.0),
             'lambda_l2': trial.suggest_uniform('lambda_l2', 1e-5, 1.0),
             'min_split_gain': trial.suggest_uniform('min_split_gain', 1e-5, 0.3),
             'bagging_freq': 0,
             'verbosity': -1
-            # 'bagging_fraction': trial.suggest_uniform('bagging_fraction', 0.5, 1.0)
+            'bagging_fraction': trial.suggest_uniform('bagging_fraction', 0.0, 1.0)
         }
 
         val_scores = []
@@ -117,7 +117,7 @@ def light_gbm_regressor(cutoff, rfe=False, rfe_feature_n=10, shapley_calc=True):
         return np.mean(val_scores)  # sum(val_scores) / len(val_scores) #median?
 
     study = optuna.create_study(direction='maximize')
-    study.optimize(objective, n_trials=200)
+    study.optimize(objective, n_trials=100)
 
     best_params = study.best_params
     best_params["objective"] = "binary"
