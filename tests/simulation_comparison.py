426e68f7e4b4da851eaa80b044651ee0766b07c6
juliusw <uimmv@student.kit.edu>
2023-12-07 11:17:38 +0100

    init

--- tests/simulation_comparison.py
+++ tests/simulation_comparison.py
@@ -5,6 +5,8 @@ from ete3 import Tree
 import pandas as pd
 from scipy.stats import skew
 import os
+
+
 def get_bipartition(node):
     if not node.is_leaf():
         left_children = sorted([leaf.name for leaf in node.children[0].iter_leaves()])
@@ -12,6 +14,8 @@ def get_bipartition(node):
         bipartition = (left_children, right_children)
         return bipartition
     return None
+
+
 folder_path = '/hits/fast/cme/wiegerjs/EBG_simulations/ebg_results/'
 
 # Get a list of folder names in the specified path
@@ -20,9 +24,9 @@ folder_names = [folder for folder in os.listdir(folder_path) if os.path.isdir(os
 results = []
 for folder_name in folder_names:
     abs_path = os.path.abspath(os.path.join(folder_path, folder_name))
-    dataset = folder_name.replace(".phy","")
+    dataset = folder_name.replace(".phy", "")
     print(abs_path)
-    tree_ebg_path = abs_path + f"/{dataset}.phy_bs_over_80_support_prediction.newick"
+    tree_ebg_path = abs_path + f"/{dataset}.phy_bs_over_70_support_prediction.newick"
     tree_true_path = f"/hits/fast/cme/wiegerjs/EBG_simulations/data/{folder_name}/gtr_g.raxml.bestTree"
 
     try:
@@ -32,14 +36,12 @@ for folder_name in folder_names:
         print(tree_ebg_path)
         continue
 
-
     try:
         tree_true = ete3.Tree(tree_true_path, format=0)
     except ete3.parser.newick.NewickError as e:
         print("True Tree broken")
         print(tree_true_path)
         continue
-
 
     branch_id_counter = 0
     for node in tree_ebg.traverse():
@@ -54,8 +56,6 @@ for folder_name in folder_names:
         if node.support is not None and not node.is_leaf():
             length = node.dist
             node.__setattr__("name", branch_id_counter)
-
-
 
     for node in tree_ebg.traverse():
         if not node.is_leaf():
@@ -74,18 +74,18 @@ for folder_name in folder_names:
                             first_match = True
                         if (bipartition_ebg[1] == bipartition_true[0]) or (bipartition_ebg[1] == bipartition_true[1]):
                             second_match = True
-                        if second_match and first_match: # bipartition is in true tree
+                        if second_match and first_match:  # bipartition is in true tree
                             bipartition_found = True
-                            if node.support >= 0.5: # bipartition is deemed significant
+                            if node.support >= 0.5:  # bipartition is deemed significant
                                 # store true positive
                                 print((node.name, node_true.name, node.support))
-                                results.append((dataset,node.name, node_true.name, node.support, "TP"))
-                            else: # bipartition is NOT deemed significant
+                                results.append((dataset, node.name, node_true.name, node.support, "TP"))
+                            else:  # bipartition is NOT deemed significant
                                 # store false negative
                                 print((node.name, node_true.name, node.support))
-                                results.append((dataset,node.name, node_true.name, node.support, "FN"))
+                                results.append((dataset, node.name, node_true.name, node.support, "FN"))
                 if not bipartition_found:
-                    if node.support >= 0.5: # store false positive
+                    if node.support >= 0.5:  # store false positive
                         print((node.name, node_true.name, node.support))
                         results.append((dataset, node.name, node_true.name, node.support, "FP"))
                     else:
