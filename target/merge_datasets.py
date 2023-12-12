7417864cc1702a64ca6870730462cf0ad2e1620f
juliusw <uimmv@student.kit.edu>
2023-12-12 20:16:26 +0100

    init

--- target/merge_datasets.py
+++ target/merge_datasets.py
@@ -213,7 +213,7 @@ loo_datasets = [value for value in dataset_list if value not in elements_to_dele
 
 for loo_dataset in loo_datasets:
     try:
-        file_path = loo_dataset + "_diff_site_stats.csv"
+        file_path = loo_dataset + "_diff_site_stats_bad.csv"
         file_path = os.path.join(os.pardir, "data/processed/features", file_path)
         df = pd.read_csv(file_path, usecols=lambda column: column != 'Unnamed: 0')
         loo_resuls_dfs.append(df)
@@ -229,6 +229,9 @@ loo_diff.drop(columns=["temp"], inplace=True, axis=1)
 
 loo_diff = loo_diff.drop_duplicates(subset=['dataset', 'sampleId'], keep='first')
 loo_resuls_combined = loo_resuls_combined.merge(loo_diff, on=["sampleId", 'dataset'], how='inner')
+
+
+
 
 
 
