b4f69e8a76fb88976dfbb6f9d047610b40d9ef18
juliusw <uimmv@student.kit.edu>
2023-12-20 13:03:24 +0100

    init

--- prediction/predictor_lgbm.py
+++ prediction/predictor_lgbm.py
@@ -44,7 +44,7 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=15, shapley_calc=True, targets=
     df = df.merge(df_pars_top, on=["dataset"], how="inner")
     df.drop(columns=["lwr_drop", "branch_dist_best_two_placements", "difficult", "current_closest_taxon_perc_ham",
                      "mean_a", "max_a", "min_a", "std_a", "mean_b", "max_b", "min_b", "std_b",
-                     "mean_a_good", "std_eig_sim", "max_a_good", "min_a_good", "std_a_good", "mean_b_good", "max_b_good", "min_b_good", "std_b_good",],
+                     "mean_a_good", "std_eig_sim", "max_a_good", "min_a_good", "std_a_good", "mean_b_good", "max_b_good", "min_b_good", "std_b_good","percentile"],
             inplace=True)
     print("Median Entropy: ")
     print(df["entropy"].median())
