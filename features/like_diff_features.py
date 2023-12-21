f7b92ab5b4063609bab6a0b372a131676a88af38
juliusw <uimmv@student.kit.edu>
2023-12-21 23:24:12 +0100

    init

--- features/like_diff_features.py
+++ features/like_diff_features.py
@@ -47,12 +47,12 @@ def query_statistics(query_filepath) -> list:
     copied_array = alignment_array.copy()
     print(copied_array.shape)
     for col in range(0, alignment_array.shape[1]):
-        print(col)
+
         if np.all(alignment_array[:, col] == '-'):
-            copied_array = np.delete(copied_array, col, axis=1)
+            alignment_array = np.delete(alignment_array, col, axis=1)
         if len(np.unique(alignment_array[:, col])) == 2:
             if np.unique(alignment_array[:, col])[0] == "-" or np.unique(alignment_array[:, col])[1] == "-":
-                copied_array = np.delete(copied_array, col, axis=1)
+                alignment_array = np.delete(alignment_array, col, axis=1)
 
                         # Identify undetermined columns (columns with only gaps or missing characters)
 
