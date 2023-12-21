0583500e9efe8c825ca623d12bcc61c3c98db3ea
juliusw <uimmv@student.kit.edu>
2023-12-21 23:21:49 +0100

    init

--- features/like_diff_features.py
+++ features/like_diff_features.py
@@ -48,8 +48,6 @@ def query_statistics(query_filepath) -> list:
     print(copied_array.shape)
     for col in range(0, alignment_array.shape[1]):
         if np.all(alignment_array[:, col] == '-'):
-            # If all elements in the column are "-", remove the column
-            print(copied_array.shape)
             copied_array = np.delete(copied_array, col, axis=1)
         if len(np.unique(alignment_array[:, col])) == 2:
             if np.unique(alignment_array[:, col])[0] == "-" or np.unique(alignment_array[:, col])[1] == "-":
