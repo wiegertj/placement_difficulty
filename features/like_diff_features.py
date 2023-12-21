a45dd98a666b017e4103acb9c12ddd0dc35db378
juliusw <uimmv@student.kit.edu>
2023-12-21 20:22:28 +0100

    init

--- features/like_diff_features.py
+++ features/like_diff_features.py
@@ -78,7 +78,6 @@ def query_statistics(query_filepath) -> list:
                 # Perform min-max scaling
                 scaled_numbers = [(x / sum(numbers)) for x in numbers]
 
-                print(len(scaled_numbers))
 
                 threshold_9 = sorted(scaled_numbers)[int(len(scaled_numbers) * 0.9)]
                 analyzed_sites_9_lik = [1 if x <= threshold_9 else 0 for x in scaled_numbers]
@@ -149,7 +148,7 @@ def query_statistics(query_filepath) -> list:
 
 
 
-        print(len(alignment[0]))
+        print(f"{len(alignment[0])}, {print(len(scaled_numbers))}")
         for position in range(len(alignment[0])):
 
             # Extract characters (residues) at the current position for all sequences
