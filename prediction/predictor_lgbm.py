ce5d45c1e50b3179ff0621c4632ca4f0fe56b0dc
juliusw <uimmv@student.kit.edu>
2023-12-21 10:12:55 +0100

    init

--- prediction/predictor_lgbm.py
+++ prediction/predictor_lgbm.py
@@ -320,7 +320,7 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=15, shapley_calc=True, targets=
             train_data = lgb.Dataset(X_train_tmp, label=y_train_tmp)
             model = lgb.train(params, train_data)
             val_preds = model.predict(X_val)
-            val_score = mean_absolute_error(np.exp(y_val), val_preds, squared=False)
+            val_score = mean_absolute_error(np.exp(y_val) - 1e10, val_preds, squared=False)
             val_scores.append(val_score)
 
         return sum(val_scores) / len(val_scores)
@@ -348,7 +348,7 @@ def light_gbm_regressor(rfe=False, rfe_feature_n=15, shapley_calc=True, targets=
     with open(model_path, 'wb') as file:
         pickle.dump(final_model, file)
 
-    y_pred = np.exp(final_model.predict(X_test.drop(axis=1, columns=["group"])))
+    y_pred = np.exp(final_model.predict(X_test.drop(axis=1, columns=["group"])) - 1e10)
 
     mse = mean_squared_error(y_test, y_pred)
     rmse = math.sqrt(mse)
