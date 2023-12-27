717466be9872ca1069097fad7076c59ea1392e40
juliusw <uimmv@student.kit.edu>
2023-12-27 09:43:02 +0100

    init

--- scripts/merge_query200.py
+++ scripts/merge_query200.py
@@ -11,6 +11,10 @@ csv_file_path_ = '/hits/fast/cme/wiegerjs/placement_difficulty/data/processed/fe
 df = pd.read_csv(csv_file_path, names=["sampleId", "dataset"])
 df_ = pd.read_csv(csv_file_path_, names=["sampleId", "dataset"])
 df = pd.concat([df, df_])
+print(df.shape)
+df = df[df.duplicated(subset=['dataset', 'sampleId'], keep=False)]
+
+
 print(df.columns)
 print(df.shape)
 
@@ -29,24 +33,19 @@ for dataset in df['dataset'].unique():
     # Iterate over sampleIds and read the corresponding FASTA files
     for sample_id in sample_ids:
         # Construct the path to the FASTA file
-        fasta_files = []
+        fasta_file_path = os.path.join(fasta_directory, f'{dataset}_query200_{sample_id}.fasta')
 
-        # Collect all fasta_file_path values with a number in the third bracket
-        for i in range(200, 450):  # Adjust the range as needed
-            fasta_file_path = os.path.join(fasta_directory, f'{dataset}_query200_{sample_id}_{i:03d}.fasta')
-            print(fasta_file_path)
-            if os.path.exists(fasta_file_path):
-                fasta_files.append(fasta_file_path)
-
-        # Read and process each valid FASTA file
-        for fasta_file_path in fasta_files:
+        # Check if the file exists before attempting to read it
+        if os.path.exists(fasta_file_path):
+            # Read the FASTA file
             records = SeqIO.parse(fasta_file_path, 'fasta')
 
             # Iterate over SeqRecord objects and append to the list
             for record in records:
                 seq_records.append(SeqRecord(Seq(str(record.seq)), id=record.id, description=''))
+
 # Path to the merged FASTA filef
-    merged_fasta_file_path = os.path.join(fasta_directory, f'merged_{dataset}_r1.fasta')
+    merged_fasta_file_path = os.path.join(fasta_directory, f'merged_{dataset}.fasta')
 
 # Write the merged SeqRecord objects to a new FASTA file
     with open(merged_fasta_file_path, 'w') as output_file:
