7245ffa3e5d9098f952624d9a8ae55a82988f113
juliusw <uimmv@student.kit.edu>
2023-12-28 16:59:17 +0100

    init

--- features/perc_hashing_msa_query.py
+++ features/perc_hashing_msa_query.py
@@ -308,7 +308,7 @@ def compute_perceptual_hash_distance(msa_file):
     sampledData = current_loo_targets[current_loo_targets["dataset"] == msa_file.replace("_reference.fasta", "")][
         "sampleId"].values.tolist()
 
-    for record_query in SeqIO.parse(os.path.join(os.pardir, "data/raw/query", query_file), 'fasta'):
+    for record_query in SeqIO.parse(os.path.join(os.pardir, "data/processed/loo/merged_" + query_file.replace("_query.fasta",".fasta")), 'fasta'):
 
         counter += 1
         if counter % 50 == 0:
