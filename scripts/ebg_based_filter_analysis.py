da7852d0dad2f046d8de396b67634dce212e6010
juliusw <uimmv@student.kit.edu>
2024-01-12 11:31:50 +0100

    init

--- scripts/ebg_based_filter_analysis.py
+++ scripts/ebg_based_filter_analysis.py
@@ -99,7 +99,7 @@ fraction_0_20 = (df_res.groupby('msa_name')['result'].max() >= 0.20).mean()
 
 print(f"Fraction of msa's with result >= 0.05: {fraction_0_05:.2%}")
 print(f"Fraction of msa's with result >= 0.10: {fraction_0_10:.2%}")
-print(f"Fraction of msa's with result >= 0.20: {fraction_0_10:.2%}")
+print(f"Fraction of msa's with result >= 0.20: {fraction_0_20:.2%}")
 
 
 # Filter DataFrame for rows with result >= 0.10
