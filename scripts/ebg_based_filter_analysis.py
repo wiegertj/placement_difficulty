199600ccb16a6b77a0770bba8214cb48a646fe6f
juliusw <uimmv@student.kit.edu>
2024-01-12 12:05:59 +0100

    init

--- scripts/ebg_based_filter_analysis.py
+++ scripts/ebg_based_filter_analysis.py
@@ -84,7 +84,7 @@ for msa_name in filenames:
                 sequence_count += 1
                 sequence_length = len(record.seq)
 
-            results.append(((sum(df_merged["prediction_taxon"])/(sum(df_merged["prediction_original"])-50)), msa_name, sequence_length, sequence_count))
+            results.append((df["prediction_median"].sum()/(ground_truth["prediction_median"].sum()-ground_truth["prediction_median"].mean()), msa_name, sequence_length, sequence_count))
 import matplotlib.pyplot as plt
 
 print(len(results))
