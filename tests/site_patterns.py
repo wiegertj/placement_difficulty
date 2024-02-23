70edf8063fbdf2c420058a2c98737e628c8cfa30
juliusw <uimmv@student.kit.edu>
2024-02-23 09:27:15 +0100

    init

--- tests/site_patterns.py
+++ tests/site_patterns.py
@@ -2,17 +2,26 @@ import pandas as pd
 import os
 import subprocess
 ebg_times = pd.read_csv("/hits/fast/cme/wiegerjs/placement_difficulty/data/processed/benchmark_ebg.csv")
-
-for msa_name in ebg_times["dataset"].tolist():
+diffs = pd.read_csv("/hits/fast/cme/wiegerjs/placement_difficulty/data/treebase_difficulty.csv")
+diffs["dataset"] = diffs["dataset"].str.replace(".phy", "")
+ebg_times = ebg_times.merge(diffs, on=["dataset"], how="inner")
+for index, row in ebg_times.iterrows():
+    msa_name = row["dataset"]
+    datatype = row["data_type"]
     msa_filepath = os.path.abspath(os.path.join(os.pardir, "data/raw/msa", msa_name + "_reference.fasta"))
 
     prefix = msa_name + "_parsing"
+    if datatype == 'AA' or datatype == "DataType.AA":
+        filler = "LG+G"
+    else:
+        filler = "GTR+G"
     raxml_command = [
         "raxml-ng",
         "--parse",
         f"--msa {msa_filepath}",
-       # "--model LG+G",
-        f"--prefix {prefix}"
+        f"--model " + filler,
+        f"--prefix {prefix}",
+        "--verbose ERROR"
 
     ]
 
