10a3af61d7b0ac76c3bc91cf181d13f08e6e34f7
juliusw <uimmv@student.kit.edu>
2024-02-23 09:34:15 +0100

    init

--- tests/site_patterns.py
+++ tests/site_patterns.py
@@ -5,6 +5,7 @@ ebg_times = pd.read_csv("/hits/fast/cme/wiegerjs/placement_difficulty/data/proce
 diffs = pd.read_csv("/hits/fast/cme/wiegerjs/placement_difficulty/data/treebase_difficulty.csv")
 diffs["dataset"] = diffs["verbose_name"].str.replace(".phy", "")
 ebg_times = ebg_times.merge(diffs, on=["dataset"], how="inner")
+results = []
 for index, row in ebg_times.iterrows():
     msa_name = row["dataset"]
     datatype = row["data_type"]
@@ -29,3 +30,27 @@ for index, row in ebg_times.iterrows():
     s = " ".join(raxml_command)
     print(s)
     subprocess.run(" ".join(raxml_command), shell=True)
+
+    import re
+
+
+    def extract_numbers(file_path):
+        with open(file_path, 'r') as file:
+            for line in file:
+                partitions_match = re.search(r'(\d+) partitions', line)
+                patterns_match = re.search(r'(\d+) patterns', line)
+                if partitions_match:
+                    partitions_number = partitions_match.group(1)
+                    print("Number of partitions:", partitions_number)
+                if patterns_match:
+                    patterns_number = patterns_match.group(1)
+                    print("Number of patterns:", patterns_number)
+
+
+    # Usage
+    file_path = "/hits/fast/cme/wiegerjs/placement_difficulty/tests/" +msa_name +"_parsing.raxml.log"  # Specify your log file path here
+    extract_numbers(file_path)
+
+    results.append((msa_name, patterns_number))
+
+print(results)
