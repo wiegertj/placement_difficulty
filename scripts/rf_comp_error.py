2d3a63da811ee72defe58e43ecbfc88e5344c20c
juliusw <uimmv@student.kit.edu>
2024-02-23 15:19:49 +0100

    init

--- scripts/rf_comp_error.py
+++ scripts/rf_comp_error.py
@@ -29,6 +29,7 @@ for file in filenames:
     subprocess.run(" ".join(raxml_command), shell=True)
 
     consensus_path = file_path_parsimonies + ".raxml.consensusTreeMRE"
+
     tree_path = os.path.join(os.pardir, "data/raw/reference_tree", file) + ".newick"
 
     print(tree_path)
@@ -37,5 +38,37 @@ for file in filenames:
                                                                                               'w') as output_file:
         output_file.write(consensus_file.read())
         output_file.write(tree_file.read())
+    combined_path = f"/hits/fast/cme/wiegerjs/placement_difficulty/tests/{file}ebg_test/ebg_tmp/{file}_combined.txt"
+    raxml_command = ["raxml-ng",
+                     "--rfdist",
+                     f"--tree {combined_path}",
+                     "--redo"]
+    # result =  subprocess.run(" ".join(raxml_command), shell=True)
+    result = subprocess.run(" ".join(raxml_command), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True,
+                            shell=True)
 
+    print("result")
+    print(result.stdout)
+    import re
+    # Check if the command was successful
+    if result.returncode == 0:
+        # Extract numbers following "set: " using regular expressions
+        numbers = re.findall(r'set:\s+(-?[\d.]+)', result.stdout)
 
+        #  # Convert the extracted strings to integers or floats
+        numbers = [int(num) if num.isdigit() else float(num) for num in numbers]
+
+        #   # Print the extracted numbers
+        print("Extracted numbers:", numbers)
+    else:
+        #   # Print an error message if the command failed
+        print("Command failed with the following error message:")
+        print(result.stderr)
+    try:
+        avg_rf_no_boot = numbers[0]
+        avg_rel_rf_no_boot = numbers[1]
+        no_top_no_boot = numbers[2]
+    except (IndexError, NameError) as e:
+        print("number extraction failed ....")
+        continue
+    ###########
