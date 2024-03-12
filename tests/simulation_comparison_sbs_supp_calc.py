40db4697d3f58df7249478ad017284b082978cef
juliusw <uimmv@student.kit.edu>
2024-03-12 20:19:42 +0100

    init

---
+++ tests/simulation_comparison_sbs_supp_calc.py
@@ -0,0 +1,41 @@
+import subprocess
+
+folder_path = '/hits/fast/cme/wiegerjs/placement_difficulty/data/processed/raxml_sbs_simulations'
+import ete3
+from ete3 import Tree
+import pandas as pd
+from scipy.stats import skew
+import os
+# Get a list of folder names in the specified path
+# List all files in the folder
+all_files = os.listdir(folder_path)
+
+# Filter files ending with ".treefile"
+tree_files = [file for file in all_files if file.__contains__('RAxML_bootstrap')]
+results = []
+for folder_name in tree_files:
+    abs_path = os.path.abspath(os.path.join(folder_path, folder_name))
+    dataset = folder_name.replace(".phy.raxml.bootstraps", "")
+    inference_path = "/hits/fast/cme/wiegerjs/EBG_simulations/raxml_inf_res/" + dataset + ".phy_boot_test_.raxml.bestTree"
+    print(inference_path)
+    tree_rb_path = abs_path
+
+
+
+    try:
+        tree_inf = ete3.Tree(inference_path, format=0)
+    except ete3.parser.newick.NewickError as e:
+        print("True Tree broken")
+        print(inference_path)
+        continue
+
+    prefix = dataset + "_final_sbs_support_"
+    raxml_command = ["raxml-ng",
+                     "--support",
+                     f"--tree {inference_path}",
+                     f"--bs-trees {abs_path}",
+                     "--redo",
+                     f"--prefix {prefix}",
+                     ]
+
+    subprocess.run(" ".join(raxml_command), shell=True)
