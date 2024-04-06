8a1dc741c914ffb1cfb855d9fd73c5d79de7a5c8
juliusw <uimmv@student.kit.edu>
2024-04-06 17:54:24 +0200

    init

--- tests/raxml_ebg_pandit.py
+++ tests/raxml_ebg_pandit.py
@@ -2,6 +2,8 @@ import time
 import pandas as pd
 import subprocess
 import os
+from Bio import Nexus
+from Bio import SeqIO
 from ete3 import Tree
 from Bio import SeqIO, AlignIO
 
@@ -19,6 +21,33 @@ for folder in df["folder"]:
     model_path = f"/hits/fast/cme/wiegerjs/placement_difficulty/tests/{folder}_pandit_inference_.raxml.bestModel"
     tree_path = f"/hits/fast/cme/wiegerjs/placement_difficulty/tests/{folder}_pandit_inference_.raxml.bestTree"
     msa_path =  f"/hits/fast/cme/wiegerjs/PANDIT/data_pandit/{folder}/data.{folder}"
+    msa_path_ebg = f"/hits/fast/cme/wiegerjs/PANDIT/data_pandit/{folder}/data.{folder}"
+
+    # Define the path for the new FASTA file
+    nexus = Nexus.Nexus()
+
+    with open(msa_path, "r") as msa_file:
+        nexus.read(msa_file)
+
+    # Extract sequences from the Nexus file
+    msa_sequences = []
+
+    for taxon, sequence in nexus.charsets.iteritems():
+        msa_sequences.append(SeqIO.SeqRecord(SeqIO.Seq(sequence), id=taxon))
+
+    # Define the path for the new FASTA file
+    fasta_path = msa_path + "_converted"
+
+    # Write the sequences to the new FASTA file
+    SeqIO.write(msa_sequences, fasta_path, "fasta")
+
+
+    if os.path.exists(fasta_path):
+        print("Conversion failed")
+        continue
+
+    print("Conversion completed successfully.")
+
 
     start_time = time.time()
     folder_name = str(folder) + "_ebg_pandit"
@@ -26,10 +55,11 @@ for folder in df["folder"]:
     raxml_command = [
         "ebg",
         f"-model {model_path}",
-        f"-msa {msa_path}",
+        f"-msa {fasta_path}",
         f"-tree {tree_path}",
         "-t b",
-        f"-o {folder_name}"
+        f"-o {folder_name}",
+        "-redo"
     ]
 
     # print("Boot")
