f2a22f6a5bc67ce8d992d79689977b4d5edf8ccb
juliusw <uimmv@student.kit.edu>
2024-12-22 20:16:58 +0100

    init

--- features/bs_change.py
+++ features/bs_change.py
@@ -1,3 +1,5 @@
+import sys
+
 import pandas as pd
 
 # Define the paths
@@ -19,14 +21,12 @@ print(df_new["std_pars_bootsupp_parents"])
 # Add suffix "_DEL" to all columns in df_new
 df_new = df_new.add_suffix('_DEL')
 
-# Perform an inner merge on 'dataset' and 'branchId'
 merged_df = df_1.merge(df_new, how='inner', left_on=['dataset', 'branchId'], right_on=['dataset_DEL', 'branchId_DEL'])
 print(merged_df["std_pars_bootstrap_support_parents"])
 # Define the column mapping
 column_mapping = {
     'mean_pars_bootstrap_support_parents': 'mean_pars_bootsupp_parents_DEL',
     'std_pars_bootstrap_support_parents': 'std_pars_bootsupp_parents_DEL',
-    'skewness_bootstrap_pars_support_tree': 'skw_pars_bootsupp_parents_DEL',
     'min_pars_bootstrap_support_children_w': 'min_pars_bootsupp_child_w_DEL',
     'max_pars_bootstrap_support_children_w': 'max_pars_bootsupp_child_w_DEL',
     'std_pars_bootstrap_support_children': 'std_pars_bootstrap_support_children_DEL'
@@ -47,6 +47,33 @@ print(merged_df.head())
 # merged_df.to_csv("/path/to/output.csv", index=False)
 
 
+print("-"*100)
 
+df_new2 = "/hits/fast/cme/wiegerjs/placement_difficulty/data/processed/features/bs_features/parsimony_1000.csv"
+df_new2 = pd.read_csv(df_new2)
+print("Old cols")
+print(df_1.columns)
+print("Old cols")
+print(df_new2.columns)
+sys.exit()
+df_new2 = df_new.add_suffix('_DEL')
+merged_df = merged_df.merge(df_new2, how='inner', left_on=['dataset', 'branchId'], right_on=['dataset_DEL', 'branchId_DEL'])
+
+
+column_mapping = {
+    'mean_pars_bootstrap_support_parents': 'mean_pars_bootsupp_parents_DEL',
+    'std_pars_bootstrap_support_parents': 'std_pars_bootsupp_parents_DEL',
+    'min_pars_bootstrap_support_children_w': 'min_pars_bootsupp_child_w_DEL',
+    'max_pars_bootstrap_support_children_w': 'max_pars_bootsupp_child_w_DEL',
+    'std_pars_bootstrap_support_children': 'std_pars_bootstrap_support_children_DEL'
+}
+
+# Replace df_1 columns with corresponding values from df_new
+for target_col, source_col in column_mapping.items():
+    if source_col in merged_df.columns:
+        merged_df[target_col] = merged_df[source_col]
+
+# Drop all columns with "_DEL" suffix
+merged_df = merged_df.loc[:, ~merged_df.columns.str.endswith('_DEL')]
 
 
